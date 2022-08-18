b042bd3ad4dfd18ae53c3671e93a2f67a6254d40
Bayological <6872903+bayological@users.noreply.github.com>
2022-08-18 15:51:32 -0500

    feat: add test function

--- contracts/Foo.sol
+++ contracts/Foo.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.16;
 
 contract Foo {
     // solhint-disable-previous-line no-empty-blocks
+
+    function testFunction() external returns (bool) {
+        return true;
+    }
 }
