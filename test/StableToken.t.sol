bb64d41cda485273572a37724f17fe34b1fbca0e
Bayological <6872903+bayological@users.noreply.github.com>
2022-09-19 15:06:41 -0500

    style: lint fixes

---
+++ test/StableToken.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.5.13;
+
+import { Test } from "celo-foundry/Test.sol";
+
+import { StableToken } from "contracts/StableToken.sol";
+import { Freezer } from "contracts/common/Freezer.sol";
+
+contract StableTokenTest is Test {
+  Freezer freezer;
+
+  function setUp() public {}
+}
