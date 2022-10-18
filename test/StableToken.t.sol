94fa7add655658748e5cbb90d1c5a32744ca929c
Nina Barbakadze <barbakadzeninaa@gmail.com>
2022-10-18 16:47:42 +0200

    test: migrated StableToken tests, precompiles library throwing error

--- test/StableToken.t.sol
+++ test/StableToken.t.sol
@@ -1,23 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.5.13;
 
-import { Test } from "celo-foundry/Test.sol";
+import { Test, console2 as console } from "celo-foundry/Test.sol";
 
 import { WithRegistry } from "./utils/WithRegistry.sol";
 
 import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import { UsingPrecompiles } from "contracts/common/UsingPrecompiles.sol";
+import { SafeMath } from "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 import { StableToken } from "contracts/StableToken.sol";
 import { Freezer } from "contracts/common/Freezer.sol";
 import { FixidityLib } from "contracts/common/FixidityLib.sol";
 
-contract StableTokenTest is Test, WithRegistry {
+// TODO test when it's frozen it reverts
+// TODO update my setting inflationv logic since it will always be outdated in some cases
+// TODO unitToValue / valueToUnit conversion
+
+contract StableTokenTest is Test, WithRegistry, UsingPrecompiles {
+  using SafeMath for uint256;
   using FixidityLib for FixidityLib.Fraction;
 
   event InflationFactorUpdated(uint256 factor, uint256 lastUpdated);
   event InflationParametersUpdated(uint256 rate, uint256 updatePeriod, uint256 lastUpdated);
   event Transfer(address indexed from, address indexed to, uint256 value);
   event TransferComment(string comment);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
   // Dependencies
   Freezer freezer;
@@ -29,6 +37,7 @@ contract StableTokenTest is Test, WithRegistry {
 
   // Global variables
   uint256 initTime;
+  uint256 inflationRate = 0.995 * 10**24;
 
   function setUp() public {
     deployer = actor("deployer");
@@ -36,28 +45,22 @@ contract StableTokenTest is Test, WithRegistry {
 
     changePrank(deployer);
 
-    // Init dependencies
     freezer = new Freezer(true);
     testee = new StableToken(true);
 
-    // Setup registry
     registry.setAddressFor("Freezer", address(freezer));
 
     address[] memory initialAddresses = new address[](0);
     uint256[] memory initialBalances = new uint256[](0);
-    testee.initialize(
-      "Celo Dollar",
-      "cUSD",
-      18,
-      address(registry),
-      FixidityLib.unwrap(FixidityLib.fixed1()),
-      1 weeks,
-      initialAddresses,
-      initialBalances,
-      "Exchange"
-    );
+    testee.initialize("Celo Dollar", "cUSD", 18, address(registry), 1e24, 1 weeks, initialAddresses, initialBalances, "Exchange");
 
     initTime = block.timestamp;
+  }
+
+  function setUpInflation(uint256 inflationRate) public {
+    testee.setInflationParameters(inflationRate, 1 weeks);
+    skip(1 weeks);
+    vm.roll(block.number + 1);
   }
 }
 
@@ -169,34 +172,39 @@ contract StableTokenTest_mint is StableTokenTest {
     vm.expectRevert("Sender not authorized to mint");
     testee.mint(notDeployer, 10000);
   }
+
+  // TODO same precompile error here
+  function test_mint_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.mint(exchange, mintAmount);
+  }
 }
 
 contract StableTokenTest_transferWithComment is StableTokenTest {
   address sender;
   address receiver;
   string comment;
-
-  uint256 inflationRate = 0.995 * 10**24;
+  uint256 mintAmount = 100 * 10**18;
 
   function setUp() public {
     super.setUp();
 
     sender = actor("sender");
     receiver = actor("receiver");
-    comment = "pineapples belong on pizza";
+    comment = "pineapples absolutely do not belong on pizza";
 
     registry.setAddressFor("Exchange", sender);
     registry.setAddressFor("Validators", sender);
     registry.setAddressFor("GrandaMento", sender);
 
     changePrank(sender);
-    testee.mint(sender, 100 * 10**18);
-  }
-
-  function setUpInflation() public {
-    testee.setInflationParameters(inflationRate, 1 weeks);
-    skip(1 weeks);
-    vm.roll(block.number + 1);
+    testee.mint(sender, mintAmount);
   }
 
   function test_transferWithComment_whenToAddressIsNull_shouldRevert() public {
@@ -204,7 +212,7 @@ contract StableTokenTest_transferWithComment is StableTokenTest {
     testee.transferWithComment(address(0), 1, comment);
   }
 
-  function test_transferWithComment_whenValueGtBalance_shouldRevert() public {
+  function test_transferWithComment_whenValueGreaterThanBalance_shouldRevert() public {
     uint256 value = IERC20(testee).balanceOf(sender) + 1;
     vm.expectRevert("transfer value exceeded balance of sender");
     testee.transferWithComment(receiver, value, comment);
@@ -229,27 +237,27 @@ contract StableTokenTest_transferWithComment is StableTokenTest {
     assertEq(receiverBalanceAfter, receiverBalanceBefore + 5);
   }
 
-  // TODO: Fraction mul precompile throws error.
-  // function test_transferWithComment_whenInflationFactorIsOutdated_shouldUpdateFactor() public {
-  //   // changePrank(deployer);
-  //   // setUpInflation();
-  //   // changePrank(sender);
-  //   // testee.transferWithComment(receiver, 5, comment);
-  //   // (uint256 rate, uint256 factor, uint256 updatePeriod, uint256 factorLastUpdated) = testee.getInflationParameters();
-  //   // assertEq(factor, inflationRate);
-  // }
+  // TODO same precompile error here
+  function test_transferWithComment_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
 
-  // function test_transferWithComment_whenInflationFactorIsOutdated_shouldEmit() public {
-  //   changePrank(deployer);
-  //   setUpInflation();
+    changePrank(sender);
 
-  //   changePrank(sender);
+    // vm.expectEmit(true, false, false, true);
+    // emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.transferWithComment(receiver, 5, comment);
+  }
 
-  //   vm.expectEmit(false, false, false, true);
-  //   emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
-  //   testee.transferWithComment(receiver, 5, comment);
-  // }
+  function test_transferWithComment_whenContractIsFrozen_shouldRevert() public {
+    changePrank(deployer);
+    freezer.freeze(address(testee));
+    vm.expectRevert("can't call when contract is frozen");
+    testee.transferWithComment(receiver, 5, comment);
+  }
+}
 
+contract StableTokenTest_setInflationParameters is StableTokenTest {
   function test_setInflationParameters_shouldUpdateParameters() public {
     uint256 newInflationRate = 2.1428571429 * 10**24;
     uint256 newUpdatePeriod = 1 weeks + 5;
@@ -272,10 +280,11 @@ contract StableTokenTest_transferWithComment is StableTokenTest {
     testee.setInflationParameters(inflationRate, newUpdatePeriod);
   }
 
-  function skip_setInflationParameters_whenFactorIsOutOfDate_shouldUpdateFactor() public {
+  function skip_setInflationParameters_whenFactorIsOutdated_shouldUpdateFactor() public {
     uint256 initialRate = 1.5 * 10**24;
     uint256 expectedFactoe = 1.5 * 10**24;
     uint256 newRate = 1 * 10**24;
+    uint256 newUpdatePeriod = 1 weeks + 5;
 
     changePrank(deployer);
 
@@ -283,8 +292,354 @@ contract StableTokenTest_transferWithComment is StableTokenTest {
     skip(1 weeks);
     vm.roll(block.number + 1);
 
-    // vm.expectEmit(false, false, false, true);
-    // emit InflationParametersUpdated(newRate, newUpdatePeriod, now);
+    vm.expectEmit(false, false, false, true);
+    emit InflationParametersUpdated(newRate, newUpdatePeriod, now);
     testee.setInflationParameters(newRate, 1 weeks);
+  }
+
+  function test_setInflationParameters_whenRateIsZero_shouldRevert() public {
+    changePrank(deployer);
+    vm.expectRevert("Must provide a non-zero inflation rate.");
+    testee.setInflationParameters(0, 1 weeks);
+  }
+}
+
+contract StableTokenTest_balanceOf is StableTokenTest {
+  uint256 mintAmount = 100 * 10**18;
+  address sender;
+
+  function setUp() public {
+    super.setUp();
+
+    sender = actor("sender");
+
+    registry.setAddressFor("Exchange", sender);
+
+    changePrank(sender);
+    testee.mint(sender, mintAmount);
+  }
+
+  function test_balanceOf_whenNoInflation_shouldFetchCorrectBalance() public {
+    uint256 balance = testee.balanceOf(sender);
+    assertEq(balance, mintAmount);
+    vm.roll(block.number + 1);
+    uint256 newBalance = testee.balanceOf(sender);
+    assertEq(newBalance, balance);
+  }
+
+  function test_balanceOf_withInflation_shouldFetchCorrectBalance() public {
+    changePrank(deployer);
+    testee.setInflationParameters(inflationRate, 1 weeks);
+    uint256 adjustedBalance = testee.balanceOf(sender);
+    assertEq(adjustedBalance, 1e20);
+  }
+}
+
+//TODO change the name and move other unit conversions here as well
+contract StableTokenTest_unitConversions is StableTokenTest {
+  function setUp() public {
+    super.setUp();
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+  }
+
+  // 0.005 depreciation
+  function test_unitsToValue_withDepreciation_shouldConvert() public {
+    testee.setInflationParameters(1e24, 1 weeks);
+    uint256 value = testee.unitsToValue(1000);
+    assertEq(value, 995);
+  }
+
+  // 0.005 depreciation twice
+  function test_unitsToValue_withDepreciationTwice_shouldConvert() public {
+    // TODO check if there's additional need for time travel
+    skip(1 weeks);
+    vm.roll(block.timestamp + 1);
+    testee.setInflationParameters(1e24, 1 weeks);
+    uint256 value = testee.unitsToValue(1000);
+    assertEq(value, 990);
+  }
+
+  // 0.005 depreciation
+  function test_valueToUnits_withDepreciation_shouldConvert() public {
+    testee.setInflationParameters(1e24, 1 weeks);
+    uint256 units = testee.valueToUnits(995);
+    assertEq(units, 999);
+  }
+
+  // 0.005 depreciation twice
+  function test_valueToUnits_withDepreciationTwice_shouldConvert() public {
+    // TODO check if there's additional need for time travel
+    testee.setInflationParameters(1e24, 1 weeks);
+    uint256 units = testee.valueToUnits(990);
+    assertEq(units, 999);
+  }
+}
+
+contract StableTokenTest_burn is StableTokenTest {
+  address exchange;
+  address grandaMento;
+
+  uint256 mintAmount = 10;
+  uint256 burnAmount = 5;
+
+  function setUp() public {
+    super.setUp();
+
+    exchange = actor("exchange");
+    grandaMento = actor("grandaMento");
+
+    registry.setAddressFor("Exchange", exchange);
+    registry.setAddressFor("GrandaMento", grandaMento);
+  }
+
+  function burnAndAssert(address to, uint256 value) public {
+    changePrank(to);
+    vm.expectEmit(true, true, true, true);
+    emit Transfer(to, address(0), testee.valueToUnits(value));
+    testee.burn(value);
+    assertEq(testee.balanceOf(to), value);
+    assertEq(testee.totalSupply(), value);
+  }
+
+  function test_burn_whenCalledByExchange_shouldBurnTokens() public {
+    changePrank(exchange);
+    testee.mint(exchange, mintAmount);
+    burnAndAssert(exchange, burnAmount);
+  }
+
+  function test_burn_whenCalledByGrandaMento_shouldBurnTokens() public {
+    changePrank(grandaMento);
+    testee.mint(grandaMento, mintAmount);
+    burnAndAssert(grandaMento, burnAmount);
+  }
+
+  function test_burn_whenValueExceedsBalance_shouldRevert() public {
+    changePrank(grandaMento);
+    testee.mint(grandaMento, mintAmount);
+    vm.expectRevert("value exceeded balance of sender");
+    testee.burn(11);
+  }
+
+  function test_burn_whenCalledByUnothorizedSender_shouldRevert() public {
+    changePrank(notDeployer);
+    vm.expectRevert("Sender not authorized to burn");
+    testee.burn(burnAmount);
+  }
+
+  // TODO same precompile error here
+  function test_burn_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.burn(burnAmount);
+  }
+}
+
+contract StableTokenTest_getExchangeRegistryId is StableTokenTest {
+  function test_getExchangeRegistryId_shouldMatchInitializedValue() public {
+    StableToken stableToken2 = new StableToken(true);
+    stableToken2.initialize(
+      "Celo Dollar",
+      "cUSD",
+      18,
+      address(registry),
+      1e24,
+      1 weeks,
+      new address[](0),
+      new uint256[](0),
+      "ExchangeEUR"
+    );
+    bytes32 fetchedId = stableToken2.getExchangeRegistryId();
+    assertEq(fetchedId, keccak256("ExchangeEUR"));
+  }
+
+  function test_whenUnitialized_shouldFallbackToDefault() public {
+    StableToken stableToken2 = new StableToken(true);
+    bytes32 fetchedId = stableToken2.getExchangeRegistryId();
+    assertEq(fetchedId, keccak256("Exchange"));
+  }
+}
+
+contract StableTokenTest_erc20Functions is StableTokenTest {
+  address exchange = actor("exchange");
+  address receiver = actor("receiver");
+  uint256 transferAmount = 1;
+  uint256 amountToMint = 10;
+
+  function setUp() public {
+    super.setUp();
+
+    registry.setAddressFor("Exchange", exchange);
+    changePrank(exchange);
+    testee.mint(exchange, amountToMint);
+  }
+
+  function assertBalance(address spenderAddress, uint256 balance) public {
+    assertEq(testee.balanceOf(spenderAddress), balance);
+  }
+
+  function test_approve_whenSpenderIsNotZeroAddress_shouldUpdateAndEmit() public {
+    vm.expectEmit(true, true, true, true);
+    emit Approval(exchange, receiver, transferAmount);
+    bool res = testee.approve(receiver, transferAmount);
+    assertEq(res, true);
+    assertEq(testee.allowance(exchange, receiver), transferAmount);
+  }
+
+  function test_approve_whenSpenderIsZeroAddress_shouldRevert() public {
+    vm.expectRevert("reserved address 0x0 cannot have allowance");
+    testee.approve(address(0), transferAmount);
+  }
+
+  // TODO same precompile error here
+  function test_approve_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+    // TODO check sender mentions
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.approve(receiver, transferAmount);
+  }
+
+  function test_increaseAllowance_whenSpenderIsNotZeroAddress_shouldUpdateAndEmit() public {
+    vm.expectEmit(true, true, true, true);
+    emit Approval(exchange, receiver, 2);
+    bool res = testee.increaseAllowance(receiver, 2);
+    assertEq(testee.allowance(exchange, receiver), 2);
+    assertEq(testee.allowance(exchange, exchange), 0);
+  }
+
+  function test_increaseAllowance_whenSpenderIsZeroAddress_shouldRevert() public {
+    vm.expectRevert("reserved address 0x0 cannot have allowance");
+    testee.increaseAllowance(address(0), transferAmount);
+  }
+
+  // TODO same precompile error here
+  function test_increaseAllowance_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+    // TODO check sender mentions
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.increaseAllowance(receiver, 2);
+  }
+
+  function test_decreaseAllowance_whenSpenderIsNotZeroAddress_shouldUpdateAndEmit() public {
+    testee.approve(receiver, 2);
+    vm.expectEmit(true, true, true, true);
+    emit Approval(exchange, receiver, transferAmount);
+    bool res = testee.decreaseAllowance(receiver, transferAmount);
+    assertEq(res, true);
+    assertEq(testee.allowance(exchange, receiver), transferAmount);
+  }
+
+  // TODO same precompile error here
+  function test_decreaseAllowance_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+    // TODO check sender mentions
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.decreaseAllowance(receiver, transferAmount);
+  }
+
+  function test_transfer_whenReceiverIsNotZeroAddress_shouldTransferAndEmit() public {
+    uint256 senderStartBalance = testee.balanceOf(exchange);
+    uint256 receiverStartBalance = testee.balanceOf(receiver);
+
+    vm.expectEmit(true, true, true, true);
+    emit Transfer(exchange, receiver, transferAmount);
+    bool res = testee.transfer(receiver, transferAmount);
+    assertEq(res, true);
+    assertBalance(exchange, senderStartBalance.sub(transferAmount));
+    assertBalance(receiver, receiverStartBalance.add(transferAmount));
+  }
+
+  function test_transfer_whenReceiverIsZeroAddress_shouldRevert() public {
+    vm.expectRevert("transfer attempted to reserved address 0x0");
+    testee.transfer(address(0), transferAmount);
+  }
+
+  function test_transfer_whenItExceedsSenderBalance_shouldRevert() public {
+    vm.expectRevert("transfer value exceeded balance of sender");
+    testee.transfer(receiver, amountToMint + 1);
+  }
+
+  function test_transfer_whenContractIsFrozen_shouldRevert() public {
+    changePrank(deployer);
+    freezer.freeze(address(testee));
+    vm.expectRevert("can't call when contract is frozen");
+    testee.transfer(receiver, amountToMint + 1);
+  }
+
+  // TODO same precompile error here
+  function test_transfer_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.transfer(receiver, transferAmount);
+  }
+
+  function test_transferFrom_whenSenderIsExchange_shouldTransferAndEmit() public {
+    testee.approve(exchange, transferAmount);
+
+    uint256 exchangeStartBalance = testee.balanceOf(exchange);
+    uint256 receiverStartBalance = testee.balanceOf(receiver);
+
+    vm.expectEmit(true, true, true, true);
+    emit Transfer(exchange, receiver, transferAmount);
+    bool res = testee.transferFrom(exchange, receiver, transferAmount);
+    assertEq(res, true);
+    assertBalance(exchange, exchangeStartBalance.sub(transferAmount));
+    assertBalance(receiver, receiverStartBalance.add(transferAmount));
+  }
+
+  function test_transferFrom_whenReceiverIsZeroAddress_shouldRevert() public {
+    vm.expectRevert("transfer attempted to reserved address 0x0");
+    testee.transferFrom(exchange, address(0), transferAmount);
+  }
+
+  function test_transferFrom_whenItExceedsSenderBalance_shouldRevert() public {
+    vm.expectRevert("transfer value exceeded balance of sender");
+    testee.transferFrom(exchange, receiver, amountToMint + 1);
+  }
+
+  function test_transferFrom_whenItExceedsSpenderAllowence_shoulrRevert() public {
+    vm.expectRevert("transfer value exceeded sender's allowance for recipient");
+    testee.transferFrom(exchange, receiver, transferAmount);
+  }
+
+  function test_transferFrom_whenContractIsFrozen_shouldRevert() public {
+    changePrank(deployer);
+    freezer.freeze(address(testee));
+    vm.expectRevert("can't call when contract is frozen");
+    testee.transferFrom(exchange, receiver, transferAmount);
+  }
+
+  // TODO same precompile error here
+  function test_transferFrom_whenInflationFactorIsOutdated_shouldUpdateAndEmit() public {
+    changePrank(deployer);
+    setUpInflation(inflationRate);
+
+    changePrank(exchange);
+
+    vm.expectEmit(true, false, false, true);
+    emit InflationParametersUpdated(inflationRate, 1 weeks + initTime, now);
+    testee.transferFrom(exchange, receiver, transferAmount);
   }
 }
