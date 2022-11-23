f659f9cf6a012073a770f606dc4ae2b06b135804
boqdan <304771+bowd@users.noreply.github.com>
2022-11-23 06:23:13 +0000

    feat(broker): trading limits (#65)
    * feat(broker): add basic trading limits library and tests
    * chore(broker): refactor test suite and add some trading limits tests
    * feat(broker): fix subunit deltaflow edge-case
    * feat(broker): guard against overflows
    * feat(broker): guard against additional overflows

--- contracts/Broker.sol
+++ contracts/Broker.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.13;
 pragma experimental ABIEncoderV2;
 
-import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 import { IExchangeProvider } from "./interfaces/IExchangeProvider.sol";
@@ -9,21 +8,30 @@ import { IBroker } from "./interfaces/IBroker.sol";
 import { IBrokerAdmin } from "./interfaces/IBrokerAdmin.sol";
 import { IReserve } from "./interfaces/IReserve.sol";
 import { IStableToken } from "./interfaces/IStableToken.sol";
+import { IERC20Metadata } from "./common/interfaces/IERC20Metadata.sol";
 
 import { Initializable } from "./common/Initializable.sol";
+import { TradingLimits } from "./common/TradingLimits.sol";
 
 /**
  * @title Broker
  * @notice The broker executes swaps and keeps track of spending limits per pair.
  */
 contract Broker is IBroker, IBrokerAdmin, Initializable, Ownable {
+  using TradingLimits for TradingLimits.State;
+  using TradingLimits for TradingLimits.Config;
+
   /* ==================== State Variables ==================== */
 
   address[] public exchangeProviders;
   mapping(address => bool) public isExchangeProvider;
+  mapping(bytes32 => TradingLimits.State) public tradingLimitsState;
+  mapping(bytes32 => TradingLimits.Config) public tradingLimitsConfig;
 
   // Address of the reserve.
   IReserve public reserve;
+
+  uint256 private constant MAX_INT256 = uint256(-1) / 2;
 
   /* ==================== Constructor ==================== */
 
@@ -146,6 +154,7 @@ contract Broker is IBroker, IBrokerAdmin, Initializable, Ownable {
     require(isExchangeProvider[exchangeProvider], "ExchangeProvider does not exist");
     amountOut = IExchangeProvider(exchangeProvider).swapIn(exchangeId, tokenIn, tokenOut, amountIn);
     require(amountOut >= amountOutMin, "amountOutMin not met");
+    guardTradingLimits(exchangeId, tokenIn, amountIn, tokenOut, amountOut);
     transferIn(msg.sender, tokenIn, amountIn);
     transferOut(msg.sender, tokenOut, amountOut);
     emit Swap(exchangeProvider, exchangeId, msg.sender, tokenIn, tokenOut, amountIn, amountOut);
@@ -172,9 +181,32 @@ contract Broker is IBroker, IBrokerAdmin, Initializable, Ownable {
     require(isExchangeProvider[exchangeProvider], "ExchangeProvider does not exist");
     amountIn = IExchangeProvider(exchangeProvider).swapOut(exchangeId, tokenIn, tokenOut, amountOut);
     require(amountIn <= amountInMax, "amountInMax exceeded");
+    guardTradingLimits(exchangeId, tokenIn, amountIn, tokenOut, amountOut);
     transferIn(msg.sender, tokenIn, amountIn);
     transferOut(msg.sender, tokenOut, amountOut);
     emit Swap(exchangeProvider, exchangeId, msg.sender, tokenIn, tokenOut, amountIn, amountOut);
+  }
+
+  /**
+   * @notice Configure trading limits for an (exchangeId, token) touple.
+   * @dev Will revert if the configuration is not valid according to the
+   * TradingLimits library.
+   * Resets existing state according to the TradingLimits library logic.
+   * Can only be called by owner.
+   * @param exchangeId the exchangeId to target.
+   * @param token the token to target.
+   * @param config the new trading limits config.
+   */
+  function configureTradingLimit(
+    bytes32 exchangeId,
+    address token,
+    TradingLimits.Config memory config
+  ) public onlyOwner {
+    config.validate();
+
+    bytes32 limitId = exchangeId ^ bytes32(uint256(uint160(token)));
+    tradingLimitsConfig[limitId] = config;
+    tradingLimitsState[limitId] = tradingLimitsState[limitId].reset(config);
   }
 
   /* ==================== Private Functions ==================== */
@@ -215,12 +247,58 @@ contract Broker is IBroker, IBrokerAdmin, Initializable, Ownable {
     uint256 amount
   ) internal {
     if (reserve.isStableAsset(token)) {
-      IERC20(token).transferFrom(from, address(this), amount);
+      IERC20Metadata(token).transferFrom(from, address(this), amount);
       IStableToken(token).burn(amount);
     } else if (reserve.isCollateralAsset(token)) {
-      IERC20(token).transferFrom(from, address(reserve), amount);
+      IERC20Metadata(token).transferFrom(from, address(reserve), amount);
     } else {
       revert("Token must be stable or collateral assert");
+    }
+  }
+
+  /**
+   * @notice Verify trading limits for a trade in both directions.
+   * @dev Reverts if the trading limits are met for outflow or inflow.
+   * @param exchangeId the ID of the exchange being used.
+   * @param _tokenIn the address of the token flowing in.
+   * @param amountIn the amount of token flowing in.
+   * @param _tokenOut the address of the token flowing out.
+   * @param amountOut  the amount of token flowing out.
+   */
+  function guardTradingLimits(
+    bytes32 exchangeId,
+    address _tokenIn,
+    uint256 amountIn,
+    address _tokenOut,
+    uint256 amountOut
+  ) internal {
+    bytes32 tokenIn = bytes32(uint256(uint160(_tokenIn)));
+    bytes32 tokenOut = bytes32(uint256(uint160(_tokenOut)));
+    require(amountIn <= uint256(MAX_INT256), "amountIn too large");
+    require(amountOut <= uint256(MAX_INT256), "amountOut too large");
+
+    guardTradingLimit(exchangeId ^ tokenIn, int256(amountIn), _tokenIn);
+    guardTradingLimit(exchangeId ^ tokenOut, -1 * int256(amountOut), _tokenOut);
+  }
+
+  /**
+   * @notice Updates and verifies a trading limit if it's configured.
+   * @dev Will revert if the trading limit is exceeded by this trade.
+   * @param tradingLimitId the ID of the trading limit associated with the token
+   * @param deltaFlow the deltaflow of this token, negative for outflow, positive for inflow.
+   * @param token the address of the token, used to lookup decimals.
+   */
+  function guardTradingLimit(
+    bytes32 tradingLimitId,
+    int256 deltaFlow,
+    address token
+  ) internal {
+    TradingLimits.Config memory tradingLimitConfig = tradingLimitsConfig[tradingLimitId];
+    if (tradingLimitConfig.flags > 0) {
+      TradingLimits.State memory tradingLimitState = tradingLimitsState[tradingLimitId];
+      tradingLimitState = tradingLimitState.update(tradingLimitConfig, deltaFlow, IERC20Metadata(token).decimals());
+      tradingLimitState.verify(tradingLimitConfig);
+      tradingLimitsState[tradingLimitId] = tradingLimitState;
     }
   }
 
