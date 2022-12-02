665c591b2cd769c9b629bb3ee5d612dde2efd9a5
philipraetsch <janphilip.raetsch@student.hpi.uni-potsdam.de>
2022-12-02 17:09:02 +0100

    fix: code formatting

--- generateTypes.js
+++ generateTypes.js
@@ -1,4 +1,4 @@
-const {runTypeChain,glob} = require('typechain')
+const { runTypeChain, glob } = require('typechain')
 const fs = require('fs');
 
 async function main() {
@@ -6,15 +6,15 @@ async function main() {
   const interfacesPath = cwd + "/contracts/interfaces"
 
   let files = await fs.readdirSync(interfacesPath)
-  files = files.map(file => { return file + "/" + file.replace("sol","json")})
-  const allFiles = glob(cwd+"/out",files)
+  files = files.map(file => { return file + "/" + file.replace("sol", "json") })
+  const allFiles = glob(cwd + "/out", files)
 
   const result = runTypeChain({
-   cwd,
-   filesToProcess: allFiles,
-   allFiles,
-   outDir: 'src',
-   target: 'ethers-v5',
-})
+    cwd,
+    filesToProcess: allFiles,
+    allFiles,
+    outDir: 'src',
+    target: 'ethers-v5',
+  })
 }
 main().catch(console.error)
