3cdaa9098f7914adfe68b5b7aa12c13971013d9c
philbow61 <philip.raetsch@gmail.com>
2023-04-25 15:05:21 +0200

    test: add tests + workflow

---
+++ test/echidna/FixidityLib.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.13;
+import "../../contracts/common/FixidityLib.sol";
+
+//echidna ./test/echidna/FixidityLib.sol --contract EchidnaFixidtyLib --config ./echidna.yaml --test-mode assertion
+  
+contract EchidnaFixidtyLib{
+
+    function wrapUnwrap(uint256 a) public returns(bool) {
+        FixidityLib.Fraction memory fraction = FixidityLib.wrap(a);
+        uint256 r = FixidityLib.unwrap(fraction);
+        assert(r == a);
+    }
+
+    function integerFractional(uint256 a) public returns(bool) {
+        FixidityLib.Fraction memory fraction = FixidityLib.Fraction(a);
+        FixidityLib.Fraction memory integer = FixidityLib.integer(fraction);
+        FixidityLib.Fraction memory fractional = FixidityLib.fractional(fraction);
+        assert(fraction.value == integer.value + fractional.value);
+    }
+
+    function addSubtract(uint256 a, uint256 b) public returns(bool) {
+        FixidityLib.Fraction memory fraction1 = FixidityLib.Fraction(a);        
+        FixidityLib.Fraction memory fraction2 = FixidityLib.Fraction(b);
+        FixidityLib.Fraction memory sum = FixidityLib.add(fraction1,fraction2);
+        FixidityLib.Fraction memory result = FixidityLib.subtract(sum, fraction2);
+        uint256 r = FixidityLib.unwrap(result);
+        assert(r == a);
+    }
+
+}
