898d973e4b508dc90a7455b7fdeb20b93bca6c4a
Bayological <6872903+bayological@users.noreply.github.com>
2023-05-19 13:00:14 -0500

    chore: add verilog tests

--- test/echidna/EchidnaConstantProductPricingModule.sol
+++ test/echidna/EchidnaConstantProductPricingModule.sol
@@ -19,123 +19,20 @@ contract EchidnaConstantProductPricingModule {
     helpers = new EchidnaHelpers();
   }
 
-  // TODO: Generate random inputs with constraints for these tests.
-
-  /* ==================== GetAmountOut ==================== */
-
-  function echidna_test_getAmountOut_zeroInputReturnsZeroOutput() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 tokenInBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountIn = 0;
-
+  function getAmounts_inverseConsistent(
+    uint256 tokenInBucketSize,
+    uint256 tokenOutBucketSize,
+    uint256 spread,
+    uint256 amountIn
+  ) public view returns (bool) {
+    spread = helpers.between(spread, 0, FixidityLib.unwrap(FixidityLib.fixed1()));
     uint256 amountOut = constantProductPricingModule.getAmountOut(
       tokenInBucketSize,
       tokenOutBucketSize,
       spread,
       amountIn
     );
-
-    assert(amountOut == 0);
-  }
-
-  function echidna_test_getAmountOut_outputLteTokenOutBucket() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 tokenInBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountIn = 500;
-
-    uint256 amountOut = constantProductPricingModule.getAmountOut(
-      tokenInBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountIn
-    );
-
-    assert(amountOut <= tokenOutBucketSize);
-  }
-
-  /* ==================== GetAmountIn ==================== */
-
-  function echidna_test_getAmountIn_zeroOutputReturnsZeroInput() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 tokenInBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountOut = 0;
-
-    uint256 amountIn = constantProductPricingModule.getAmountIn(
-      tokenInBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountOut
-    );
-
-    assert(amountIn == 0);
-  }
-
-  function echidna_test_getAmountIn_outputLteTokenOutBucket() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 tokenInBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountOut = 500;
-
-    uint256 amountIn = constantProductPricingModule.getAmountIn(
-      tokenInBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountOut
-    );
-
-    assert(amountIn <= tokenOutBucketSize);
-  }
-
-  /* ==================== Inverse Operations ==================== */
-
-  function echidna_test_getAmountOut_getAmountIn_inverse() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 tokenInBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountIn = 5000;
-
-    spread = helpers.between(spread, tokenInBucketSize, FixidityLib.unwrap(FixidityLib.fixed1()));
-
-    uint256 amountOut = constantProductPricingModule.getAmountOut(
-      tokenOutBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountIn
-    );
-    uint256 amountInCalculated = constantProductPricingModule.getAmountIn(
-      tokenOutBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountOut
-    );
-
-    assert(amountInCalculated == amountIn);
-  }
-
-  function echidna_test_getAmountIn_getAmountOut_inverse() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 tokenInBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountOut = 5000;
-
-    spread = helpers.between(spread, tokenInBucketSize, FixidityLib.unwrap(FixidityLib.fixed1()));
-
-    uint256 amountIn = constantProductPricingModule.getAmountIn(
-      tokenOutBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountOut
-    );
-    uint256 amountOutCalculated = constantProductPricingModule.getAmountOut(
-      tokenOutBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountIn
-    );
-
-    assert(amountOutCalculated == amountOut);
+    uint256 r = constantProductPricingModule.getAmountIn(tokenInBucketSize, tokenOutBucketSize, spread, amountOut);
+    return (r == amountIn);
   }
 }
--- test/echidna/EchidnaConstantSumPricingModule.sol
+++ test/echidna/EchidnaConstantSumPricingModule.sol
@@ -19,97 +19,14 @@ contract EchidnaConstantSumPricingModule {
     helpers = new EchidnaHelpers();
   }
 
-  // TODO: Generate random inputs with constraints for these tests.
-
-  /* ==================== GetAmountOut ==================== */
-
-  function echidna_test_getAmountOut_zeroInputReturnsZeroOutput() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountIn = 0;
-
-    uint256 amountOut = constantSumPricingModule.getAmountOut(0, tokenOutBucketSize, spread, amountIn);
-
-    assert(amountOut == 0);
-  }
-
-  function echidna_test_getAmountOut_outputLteTokenOutBucket() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountIn = 500;
-
-    uint256 amountOut = constantSumPricingModule.getAmountOut(0, tokenOutBucketSize, spread, amountIn);
-
-    assert(amountOut <= tokenOutBucketSize);
-  }
-
-  /* ==================== GetAmountIn ==================== */
-
-  function echidna_test_getAmountIn_zeroOutputReturnsZeroInput() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountOut = 0;
-
-    uint256 amountIn = constantSumPricingModule.getAmountIn(0, tokenOutBucketSize, spread, amountOut);
-
-    assert(amountIn == 0);
-  }
-
-  function echidna_test_getAmountIn_outputLteTokenOutBucket() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountOut = 500;
-
-    uint256 amountIn = constantSumPricingModule.getAmountIn(0, tokenOutBucketSize, spread, amountOut);
-
-    assert(amountIn <= tokenOutBucketSize);
-  }
-
-  /* ==================== Inverse Operations ==================== */
-
-  function echidna_test_getAmountOut_getAmountIn_inverse(
+  function getAmounts_inverseConsistent(
     uint256 tokenOutBucketSize,
     uint256 spread,
     uint256 amountIn
-  ) public view {
-    // uint256 tokenOutBucketSize = 10000;
-    // uint256 spread = 500;
-    // uint256 amountIn = 5000;
-
-    tokenOutBucketSize = (tokenOutBucketSize % 10000) + 1;
-    spread = spread % 1000;
-    amountIn = (amountIn % 10000) + 1;
-
-    // Make sure spread is between 0 and 1.
+  ) public view returns (bool) {
     spread = helpers.between(spread, 0, FixidityLib.unwrap(FixidityLib.fixed1()));
-
     uint256 amountOut = constantSumPricingModule.getAmountOut(tokenOutBucketSize, tokenOutBucketSize, spread, amountIn);
-    uint256 amountInCalculated = constantSumPricingModule.getAmountIn(
-      tokenOutBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountOut
-    );
-
-    assert(amountInCalculated == amountIn);
-  }
-
-  function echidna_test_getAmountIn_getAmountOut_inverse() public view {
-    uint256 tokenOutBucketSize = 10000;
-    uint256 spread = 500;
-    uint256 amountOut = 5000;
-
-    // Make sure spread is between 0 and 1.
-    spread = helpers.between(spread, 0, FixidityLib.unwrap(FixidityLib.fixed1()));
-
-    uint256 amountIn = constantSumPricingModule.getAmountIn(tokenOutBucketSize, tokenOutBucketSize, spread, amountOut);
-    uint256 amountOutCalculated = constantSumPricingModule.getAmountOut(
-      tokenOutBucketSize,
-      tokenOutBucketSize,
-      spread,
-      amountIn
-    );
-
-    assert(amountOutCalculated == amountOut);
+    uint256 r = constantSumPricingModule.getAmountIn(tokenOutBucketSize, tokenOutBucketSize, spread, amountOut);
+    return (r == amountIn);
   }
 }
