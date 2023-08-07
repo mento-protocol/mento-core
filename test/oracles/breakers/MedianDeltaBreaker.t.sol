80f9c9d52d1ad7ce694a7a1f28d46a89380dab62
Nina Barbakadze <barbakadzeninaa@gmail.com>
2023-08-07 11:59:06 +0200

    style: lint

--- test/oracles/breakers/MedianDeltaBreaker.t.sol
+++ test/oracles/breakers/MedianDeltaBreaker.t.sol
@@ -24,7 +24,7 @@ contract MedianDeltaBreakerTest is BaseTest {
   MockSortedOracles sortedOracles;
   MedianDeltaBreaker breaker;
 
-  uint256 defaultThreshold = 0.15 * 10 ** 24; // 15%
+  uint256 defaultThreshold = 0.15 * 10**24; // 15%
   uint256 defaultCooldownTime = 5 minutes;
 
   address[] rateFeedIDs = new address[](1);
@@ -49,7 +49,7 @@ contract MedianDeltaBreakerTest is BaseTest {
     breakerBox = actor("breakerBox");
 
     rateFeedIDs[0] = rateFeedID2;
-    rateChangeThresholds[0] = 0.9 * 10 ** 24;
+    rateChangeThresholds[0] = 0.9 * 10**24;
     cooldownTimes[0] = 10 minutes;
 
     vm.startPrank(deployer);
@@ -126,11 +126,11 @@ contract MedianDeltaBreakerTest_constructorAndSetters is MedianDeltaBreakerTest 
 
   function test_setRateChangeThreshold_whenValueGreaterThanOne_shouldRevert() public {
     vm.expectRevert("value must be less than 1");
-    breaker.setDefaultRateChangeThreshold(1 * 10 ** 24);
+    breaker.setDefaultRateChangeThreshold(1 * 10**24);
   }
 
   function test_setRateChangeThreshold_whenCallerIsOwner_shouldUpdateAndEmit() public {
-    uint256 testThreshold = 0.1 * 10 ** 24;
+    uint256 testThreshold = 0.1 * 10**24;
     vm.expectEmit(false, false, false, true);
     emit DefaultRateChangeThresholdUpdated(testThreshold);
 
@@ -196,7 +196,7 @@ contract MedianDeltaBreakerTest_constructorAndSetters is MedianDeltaBreakerTest 
   }
 
   function test_setRateChangeThreshold_whenThresholdIsExactly1_shouldRevert() public {
-    rateChangeThresholds[0] = 1 * 10 ** 24;
+    rateChangeThresholds[0] = 1 * 10**24;
     vm.expectRevert("value must be less than 1");
     breaker.setRateChangeThresholds(rateFeedIDs, rateChangeThresholds);
   }
@@ -259,8 +259,8 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
   }
 
   function updatePreviousEMAByPercent(uint256 medianChangeScaleFactor, address _rateFeedID) public {
-    uint256 previousEMA = 0.98 * 10 ** 24;
-    uint256 currentMedianRate = (previousEMA * medianChangeScaleFactor) / 10 ** 24;
+    uint256 previousEMA = 0.98 * 10**24;
+    uint256 currentMedianRate = (previousEMA * medianChangeScaleFactor) / 10**24;
     stdstore.target(address(breaker)).sig(breaker.medianRatesEMA.selector).with_key(_rateFeedID).checked_write(
       previousEMA
     );
@@ -277,15 +277,15 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
 
   function test_shouldTrigger_withDefaultThreshold_shouldTrigger() public {
     assertEq(breaker.rateChangeThreshold(rateFeedID1), 0);
-    updatePreviousEMAByPercent(0.7 * 10 ** 24, rateFeedID1);
+    updatePreviousEMAByPercent(0.7 * 10**24, rateFeedID1);
     changePrank(breakerBox);
     assertTrue(breaker.shouldTrigger(rateFeedID1));
   }
 
   function test_shouldTrigger_whenThresholdIsLargerThanMedian_shouldNotTrigger() public {
-    updatePreviousEMAByPercent(0.7 * 10 ** 24, rateFeedID1);
+    updatePreviousEMAByPercent(0.7 * 10**24, rateFeedID1);
 
-    rateChangeThresholds[0] = 0.8 * 10 ** 24;
+    rateChangeThresholds[0] = 0.8 * 10**24;
     rateFeedIDs[0] = rateFeedID1;
     breaker.setRateChangeThresholds(rateFeedIDs, rateChangeThresholds);
     assertEq(breaker.rateChangeThreshold(rateFeedID1), rateChangeThresholds[0]);
@@ -297,15 +297,15 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
   function test_shouldTrigger_whithDefaultThreshold_ShouldNotTrigger() public {
     assertEq(breaker.rateChangeThreshold(rateFeedID3), 0);
 
-    updatePreviousEMAByPercent(1.1 * 10 ** 24, rateFeedID3);
+    updatePreviousEMAByPercent(1.1 * 10**24, rateFeedID3);
 
     changePrank(breakerBox);
     assertFalse(breaker.shouldTrigger(rateFeedID3));
   }
 
   function test_shouldTrigger_whenThresholdIsSmallerThanMedian_ShouldTrigger() public {
-    updatePreviousEMAByPercent(1.1 * 10 ** 24, rateFeedID3);
-    rateChangeThresholds[0] = 0.01 * 10 ** 24;
+    updatePreviousEMAByPercent(1.1 * 10**24, rateFeedID3);
+    rateChangeThresholds[0] = 0.01 * 10**24;
     rateFeedIDs[0] = rateFeedID3;
     breaker.setRateChangeThresholds(rateFeedIDs, rateChangeThresholds);
     assertEq(breaker.rateChangeThreshold(rateFeedID3), rateChangeThresholds[0]);
@@ -321,7 +321,7 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
     (uint256 beforeRate, ) = sortedOracles.medianRate(rateFeed);
     assertEq(beforeRate, 0);
 
-    uint256 median = 0.9836 * 10 ** 24;
+    uint256 median = 0.9836 * 10**24;
     setSortedOraclesMedian(median);
 
     (uint256 afterRate, ) = sortedOracles.medianRate(rateFeed);
@@ -334,25 +334,25 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
 
   function test_shouldTrigger_whenMedianDrops_shouldCalculateEMACorrectlyAndTrigger() public {
     address rateFeed = rateFeedIDs[0];
-    uint256 smoothingFactor = 0.1 * 10 ** 24;
-    rateChangeThresholds[0] = 0.03 * 10 ** 24;
+    uint256 smoothingFactor = 0.1 * 10**24;
+    rateChangeThresholds[0] = 0.03 * 10**24;
     rateFeedIDs[0] = rateFeed;
     breaker.setSmoothingFactor(rateFeed, smoothingFactor);
     breaker.setRateChangeThresholds(rateFeedIDs, rateChangeThresholds);
 
-    uint256 firstMedian = 1.05 * 10 ** 24;
+    uint256 firstMedian = 1.05 * 10**24;
     setSortedOraclesMedian(firstMedian);
     changePrank(breakerBox);
     assertFalse(breaker.shouldTrigger(rateFeed));
     assertEq(breaker.medianRatesEMA(rateFeed), firstMedian);
 
-    uint256 secondMedian = 1.0164 * 10 ** 24;
+    uint256 secondMedian = 1.0164 * 10**24;
     setSortedOraclesMedian(secondMedian);
     changePrank(breakerBox);
     bool triggered = breaker.shouldTrigger((rateFeed));
 
     // 0.1*1.0164 + (1.05 * 0.9) = 1.04664
-    assertEq(breaker.medianRatesEMA(rateFeed), 1.04664 * 10 ** 24);
+    assertEq(breaker.medianRatesEMA(rateFeed), 1.04664 * 10**24);
 
     // (1.0164-1.05)/1.05 = -0.03200000000000007
     assertTrue(triggered);
@@ -360,25 +360,25 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
 
   function test_shouldTrigger_whenMedianJumps_shouldCalculateEMACorrectlyAndTrigger() public {
     address rateFeed = rateFeedIDs[0];
-    uint256 smoothingFactor = 0.1 * 10 ** 24;
-    rateChangeThresholds[0] = 0.03 * 10 ** 24;
+    uint256 smoothingFactor = 0.1 * 10**24;
+    rateChangeThresholds[0] = 0.03 * 10**24;
     rateFeedIDs[0] = rateFeed;
     breaker.setSmoothingFactor(rateFeed, smoothingFactor);
     breaker.setRateChangeThresholds(rateFeedIDs, rateChangeThresholds);
 
-    uint256 firstMedian = 1.05 * 10 ** 24;
+    uint256 firstMedian = 1.05 * 10**24;
     setSortedOraclesMedian(firstMedian);
     changePrank(breakerBox);
     assertFalse(breaker.shouldTrigger(rateFeed));
     assertEq(breaker.medianRatesEMA(rateFeed), firstMedian);
 
-    uint256 secondMedian = 1.0836 * 10 ** 24;
+    uint256 secondMedian = 1.0836 * 10**24;
     setSortedOraclesMedian(secondMedian);
     changePrank(breakerBox);
     bool triggered = breaker.shouldTrigger((rateFeed));
 
     // 0.1*1.0836 + (1.05 * 0.9) = 1.05336
-    assertEq(breaker.medianRatesEMA(rateFeed), 1.05336 * 10 ** 24);
+    assertEq(breaker.medianRatesEMA(rateFeed), 1.05336 * 10**24);
 
     // (1.0836-1.05)/1.05 = 0.031999999999999855
     assertTrue(triggered);
@@ -388,11 +388,11 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
     address rateFeed = rateFeedIDs[0];
 
     uint256[5] memory medians;
-    medians[0] = 0.997 * 10 ** 24;
-    medians[1] = 0.9968 * 10 ** 24;
-    medians[2] = 0.9769 * 10 ** 24;
-    medians[3] = 0.9759 * 10 ** 24;
-    medians[4] = 0.9854 * 10 ** 24;
+    medians[0] = 0.997 * 10**24;
+    medians[1] = 0.9968 * 10**24;
+    medians[2] = 0.9769 * 10**24;
+    medians[3] = 0.9759 * 10**24;
+    medians[4] = 0.9854 * 10**24;
 
     for (uint256 i = 0; i < medians.length; i++) {
       setSortedOraclesMedian(medians[i]);
@@ -404,22 +404,22 @@ contract MedianDeltaBreakerTest_shouldTrigger is MedianDeltaBreakerTest {
 
   function test_shouldTrigger_withLongSequencesOfUpdates_shouldCalculateEMACorrectly() public {
     address rateFeed = rateFeedIDs[0];
-    uint256 smoothingFactor = 0.1 * 10 ** 24;
+    uint256 smoothingFactor = 0.1 * 10**24;
     breaker.setSmoothingFactor(rateFeed, smoothingFactor);
 
     uint256[5] memory medians;
-    medians[0] = 0.997 * 10 ** 24;
-    medians[1] = 0.9968 * 10 ** 24;
-    medians[2] = 0.9769 * 10 ** 24;
-    medians[3] = 0.9759 * 10 ** 24;
-    medians[4] = 0.9854 * 10 ** 24;
+    medians[0] = 0.997 * 10**24;
+    medians[1] = 0.9968 * 10**24;
+    medians[2] = 0.9769 * 10**24;
+    medians[3] = 0.9759 * 10**24;
+    medians[4] = 0.9854 * 10**24;
 
     uint256[5] memory expectedEMAs;
-    expectedEMAs[0] = 0.997 * 10 ** 24;
-    expectedEMAs[1] = 0.99698 * 10 ** 24;
-    expectedEMAs[2] = 0.994972 * 10 ** 24;
-    expectedEMAs[3] = 0.9930648 * 10 ** 24;
-    expectedEMAs[4] = 0.99229832 * 10 ** 24;
+    expectedEMAs[0] = 0.997 * 10**24;
+    expectedEMAs[1] = 0.99698 * 10**24;
+    expectedEMAs[2] = 0.994972 * 10**24;
+    expectedEMAs[3] = 0.9930648 * 10**24;
+    expectedEMAs[4] = 0.99229832 * 10**24;
 
     for (uint256 i = 0; i < medians.length; i++) {
       setSortedOraclesMedian(medians[i]);
