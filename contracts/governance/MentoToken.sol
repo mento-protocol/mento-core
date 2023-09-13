e21433b1a1716575ca88137c3bde78bb5f8946c7
baroooo <baranseltekin@gmail.com>
2023-09-13 16:42:14 +0200

    feat: Mint function for emission contract (#267)

--- contracts/governance/MentoToken.sol
+++ contracts/governance/MentoToken.sol
@@ -10,35 +10,56 @@ import { ERC20Burnable } from "openzeppelin-contracts-next/contracts/token/ERC20
  * @notice This contract represents the Mento Protocol Token which is a Burnable ERC20 token.
  */
 contract MentoToken is ERC20, ERC20Burnable {
+  /// @notice The address of the emission contract that has the capability to emit new tokens.
+  address public immutable emissionContract;
+
+  /// @notice The total amount of tokens that can be minted by the emission contract.
+  uint256 public immutable emissionSupply;
+
+  /// @notice The total amount of tokens that have been minted by the emission contract so far.
+  uint256 public emittedAmount;
+
   /**
    * @dev Constructor for the MentoToken contract.
    * @notice It mints and allocates the initial token supply among several contracts.
    * @param vestingContract The address of the vesting contract where 20% of the total supply will be sent.
    * @param airgrabContract The address of the airgrab contract where 5% of the total supply will be sent.
    * @param treasuryContract The address of the treasury contract where 10% of the total supply will be sent.
-   * @param emissionContract The address of the emission contract where 65% of the total supply will be sent.
+   * @param emissionContract_ The address of the emission contract where the rest of the supply will be emitted.
    */
   constructor(
     address vestingContract,
     address airgrabContract,
     address treasuryContract,
-    address emissionContract
-  )
-    ERC20("Mento Token", "MENTO") // Initializes the ERC20 token with name and symbol
-  {
-    // Define the initial total supply as 1 billion tokens
+    address emissionContract_
+  ) ERC20("Mento Token", "MENTO") {
     uint256 supply = 1_000_000_000 * 10**decimals();
 
-    // Calculate the allocations for different purposes based on initial supply
-    uint256 vestingSupply = (supply * 20) / 100; // 20%
-    uint256 airgrabSupply = (supply * 5) / 100; // 5%
-    uint256 treasurySupply = (supply * 10) / 100; // 10%
-    uint256 emissionSupply = (supply * 65) / 100; // 65%
+    uint256 vestingSupply = (supply * 20) / 100;
+    uint256 airgrabSupply = (supply * 5) / 100;
+    uint256 treasurySupply = (supply * 10) / 100;
+    uint256 emissionSupply_ = (supply * 65) / 100;
 
-    // Mint the tokens to respective contracts
     _mint(vestingContract, vestingSupply);
     _mint(airgrabContract, airgrabSupply);
     _mint(treasuryContract, treasurySupply);
-    _mint(emissionContract, emissionSupply);
+
+    emissionContract = emissionContract_;
+    emissionSupply = emissionSupply_;
+  }
+
+  /**
+   * @dev Allows the emission contract to mint new tokens up to the emission supply limit.
+   * @notice This function can only be called by the emission contract and
+   * only if the total emitted amount hasn't exceeded the emission supply.
+   * @param target Address to which the newly minted tokens will be sent.
+   * @param amount Amount of tokens to be minted.
+   */
+  function mint(address target, uint256 amount) external {
+    require(msg.sender == emissionContract, "MentoToken: only emission contract");
+    require(emittedAmount + amount <= emissionSupply, "MentoToken: emission supply exceeded");
+
+    emittedAmount += amount;
+    _mint(target, amount);
   }
 }
--- test/governance/MentoToken.t.sol
+++ test/governance/MentoToken.t.sol
@@ -1,6 +1,6 @@
 // solhint-disable func-name-mixedcase
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
 import { console } from "forge-std-next/console.sol";
 import { Test } from "forge-std-next/Test.sol";
@@ -18,82 +18,137 @@ contract MentoTokenTest is Test {
   address public constant ALICE = address(9999);
   address public constant BOB = address(8888);
 
-  uint256 public constant INITIAL_TOTAL_SUPPLY = 1_000_000_000 * 1e18;
+  uint256 public constant INITIAL_TOTAL_SUPPLY = 350_000_000 * 1e18;
+  uint256 public constant EMISSION_SUPPLY = 650_000_000 * 1e18;
 
   function setUp() public {
     mentoToken = new MentoToken(VESTING_CONTRACT, AIRGRAB_CONTRACT, TREASURY_CONTRACT, EMISSION_CONTRACT);
   }
 
+  /// @dev Test the state initialization post-construction of the MentoToken contract.
+  function test_constructor_shouldSetCorrectState() public {
+    assertEq(mentoToken.emissionContract(), EMISSION_CONTRACT);
+    assertEq(mentoToken.emissionSupply(), EMISSION_SUPPLY);
+    assertEq(mentoToken.emittedAmount(), 0);
+  }
+
   /// @dev Test the correct token amounts are minted to respective contracts during initialization.
   function test_constructor_shouldMintCorrectAmounts() public {
-    // Fetch the balances of all the addresses set during contract initialization
     uint256 vestingAmount = mentoToken.balanceOf(VESTING_CONTRACT);
     uint256 airgrabAmount = mentoToken.balanceOf(AIRGRAB_CONTRACT);
     uint256 treasuryAmount = mentoToken.balanceOf(TREASURY_CONTRACT);
     uint256 emissionAmount = mentoToken.balanceOf(EMISSION_CONTRACT);
 
-    // Assert that each contract has the expected amount of tokens
     assertEq(vestingAmount, 200_000_000 * 1e18);
     assertEq(airgrabAmount, 50_000_000 * 1e18);
     assertEq(treasuryAmount, 100_000_000 * 1e18);
-    assertEq(emissionAmount, 650_000_000 * 1e18);
+    assertEq(emissionAmount, 0);
 
-    // Assert that the total token minted during initialization matches the sum of tokens assigned to each contract
     assertEq(vestingAmount + airgrabAmount + treasuryAmount + emissionAmount, INITIAL_TOTAL_SUPPLY);
-    // Assert that the token's total supply matches the predefined initial total supply
     assertEq(mentoToken.totalSupply(), INITIAL_TOTAL_SUPPLY);
   }
 
-  /// @dev Test the burn functionality for an individual account.
-  /// @notice Even though the burn function comes from OpenZeppelin's library,
-  /// @notice this test assures correct integration.
+  /**
+   * @dev Test the burn functionality for an individual account.
+   * @notice Even though the burn function comes from OpenZeppelin's library,
+   * @notice this test assures correct integration.
+   */
   function test_burn_shouldBurnTokens() public {
-    // Set up initial parameters and balances
     uint256 initialBalance = 3e18;
     uint256 burnAmount = 1e18;
     deal(address(mentoToken), ALICE, initialBalance);
 
-    // Expect a revert since ALICE is trying to burn more tokens than their balance
     vm.startPrank(ALICE);
     vm.expectRevert("ERC20: burn amount exceeds balance");
     mentoToken.burn(initialBalance + 1);
 
-    // Successfully burn tokens and assert the results
     mentoToken.burn(burnAmount);
     assertEq(mentoToken.balanceOf(ALICE), initialBalance - burnAmount);
     assertEq(mentoToken.totalSupply(), INITIAL_TOTAL_SUPPLY - burnAmount);
   }
 
-  /// @dev Test the burnFrom functionality considering allowances.
-  /// @notice Even though the burnFrom function comes from OpenZeppelin's library,
-  /// @notice this test assures correct integration.
-  function test_burnFrom_shouldBurnTokens_upToAllowance() public {
-    // Set up initial parameters and balances
+  /**
+   * @dev Test the burnFrom functionality considering allowances.
+   * @notice Even though the burnFrom function comes from OpenZeppelin's library,
+   * @notice this test assures correct integration.
+   */
+  function test_burnFrom_whenAllowed_shouldBurnTokens() public {
     uint256 initialBalance = 3e18;
     uint256 burnAmount = 1e18;
     deal(address(mentoToken), ALICE, initialBalance);
 
-    // BOB tries to burn ALICE's tokens without any allowance. This should fail.
     vm.prank(BOB);
     vm.expectRevert("ERC20: insufficient allowance");
     mentoToken.burnFrom(ALICE, burnAmount);
 
-    // ALICE approves BOB for a specific amount of their tokens
     vm.prank(ALICE);
     mentoToken.approve(BOB, burnAmount);
 
-    // BOB tries to burn more tokens than the allowance. This should fail.
     vm.startPrank(BOB);
     vm.expectRevert("ERC20: insufficient allowance");
     mentoToken.burnFrom(ALICE, burnAmount + 1);
 
-    // BOB successfully burns up to the allowed amount of ALICE's tokens and asserts the results
     mentoToken.burnFrom(ALICE, burnAmount);
     assertEq(mentoToken.balanceOf(ALICE), initialBalance - burnAmount);
     assertEq(mentoToken.totalSupply(), INITIAL_TOTAL_SUPPLY - burnAmount);
 
-    // BOB tries to burn again, but the allowance is now exhausted. This should fail.
     vm.expectRevert("ERC20: insufficient allowance");
     mentoToken.burnFrom(ALICE, burnAmount);
+  }
+
+  /**
+   * @dev Tests the mint function's access control mechanism.
+   * @dev This test ensures that the mint function can only be called by the emission contract address.
+   * Any other address attempting to mint tokens should have the transaction reverted.
+   */
+  function test_mint_whenNotEmissionContract_shouldRevert() public {
+    uint256 mintAmount = 10e18;
+    vm.prank(BOB);
+    vm.expectRevert("MentoToken: only emission contract");
+    mentoToken.mint(ALICE, mintAmount);
+  }
+
+  /**
+   * @dev Tests the mint function's behavior when minting amounts that exceed the emission supply.
+   * @notice This test ensures that when the mint function is called with an amount that
+   * exceeds the total emission supply, the transaction should be reverted.
+   */
+  function test_mint_whenAmountBiggerThanEmissionSupply_shouldRevert() public {
+    uint256 mintAmount = 10e18;
+
+    vm.startPrank(EMISSION_CONTRACT);
+
+    vm.expectRevert("MentoToken: emission supply exceeded");
+    mentoToken.mint(ALICE, EMISSION_SUPPLY + 1);
+
+    mentoToken.mint(ALICE, mintAmount);
+
+    vm.expectRevert("MentoToken: emission supply exceeded");
+    mentoToken.mint(ALICE, EMISSION_SUPPLY - mintAmount + 1);
+  }
+
+  /**
+   * @dev Tests the mint function's logic for the emission contract.
+   * @notice This test checks:
+   * 1. Tokens can be successfully minted to specific addresses when called by the emission contract.
+   * 2. The emittedAmount state variable correctly reflects the total amount of tokens emitted.
+   * 3. It can mint up to emission supply
+   */
+  function test_mint_whenEmissionSupplyNotExceeded_shouldEmitTokens() public {
+    uint256 mintAmount = 10e18;
+
+    vm.startPrank(EMISSION_CONTRACT);
+    mentoToken.mint(ALICE, mintAmount);
+
+    assertEq(mentoToken.balanceOf(ALICE), mintAmount);
+    assertEq(mentoToken.emittedAmount(), mintAmount);
+
+    mentoToken.mint(BOB, mintAmount);
+
+    assertEq(mentoToken.balanceOf(BOB), mintAmount);
+    assertEq(mentoToken.emittedAmount(), 2 * mintAmount);
+
+    mentoToken.mint(ALICE, EMISSION_SUPPLY - 2 * mintAmount);
+    assertEq(mentoToken.emittedAmount(), EMISSION_SUPPLY);
   }
 }
