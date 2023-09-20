6ec03c94734869e831a52d35d5bfa650a2d58073
philbow61 <philip.raetsch@gmail.com>
2023-09-20 12:41:28 +0200

    feat: import raris locking contracts

---
+++ contracts/governance/LockingBase.sol
@@ -0,0 +1,437 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "openzeppelin-solidity-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
+import "openzeppelin-solidity-upgradeable/contracts/access/OwnableUpgradeable.sol";
+
+import "./libs/LibBrokenLine.sol";
+
+import "./interfaces/IVotesUpgradeable.sol";
+
+abstract contract LockingBase is OwnableUpgradeable, IVotesUpgradeable {
+  using LibBrokenLine for LibBrokenLine.BrokenLine;
+
+  uint32 public constant WEEK = 50400; //blocks one week = 50400, day = 7200, goerli = 50
+
+  uint32 constant MAX_CLIFF_PERIOD = 103;
+  uint32 constant MAX_SLOPE_PERIOD = 104;
+
+  uint32 constant ST_FORMULA_DIVIDER = 1 * (10**8); //stFormula divider          100000000
+  uint32 constant ST_FORMULA_CONST_MULTIPLIER = 2 * (10**7); //stFormula const multiplier  20000000
+  uint32 constant ST_FORMULA_CLIFF_MULTIPLIER = 8 * (10**7); //stFormula cliff multiplier  80000000
+  uint32 constant ST_FORMULA_SLOPE_MULTIPLIER = 4 * (10**7); //stFormula slope multiplier  40000000
+
+  /**
+   * @dev ERC20 token to lock
+   */
+  IERC20Upgradeable public token;
+  /**
+   * @dev counter for Lock identifiers
+   */
+  uint256 public counter;
+
+  /**
+   * @dev true if contract entered stopped state
+   */
+  bool public stopped;
+
+  /**
+   * @dev address to migrate Locks to (zero if not in migration state)
+   */
+  address public migrateTo;
+
+  /**
+   * @dev minimal cliff period in weeks, minCliffPeriod < MAX_CLIFF_PERIOD
+   */
+
+  uint256 public minCliffPeriod;
+
+  /**
+   * @dev minimal slope period in weeks, minSlopePeriod < MAX_SLOPE_PERIOD
+   */
+  uint256 public minSlopePeriod;
+
+  /**
+   * @dev locking epoch start in weeks
+   */
+  uint256 public startingPointWeek;
+
+  /**
+   * @dev represents one user Lock
+   */
+  struct Lock {
+    address account;
+    address delegate;
+  }
+
+  /**
+   * @dev describes state of accounts's balance.
+   *      balance - broken line describes lock
+   *      locked - broken line describes how many tokens are locked
+   *      amount - total currently locked tokens (including tokens which can be withdrawed)
+   */
+  struct AccountOld {
+    LibBrokenLine.BrokenLineOld balance;
+    LibBrokenLine.BrokenLineOld locked;
+    uint256 amount;
+  }
+
+  mapping(address => AccountOld) accountsOld;
+  mapping(uint256 => Lock) locks;
+  LibBrokenLine.BrokenLineOld public totalSupplyLineOld;
+
+  struct Account {
+    LibBrokenLine.BrokenLine balance;
+    LibBrokenLine.BrokenLine locked;
+    uint96 amount;
+  }
+
+  mapping(address => Account) accounts;
+  LibBrokenLine.BrokenLine public totalSupplyLine;
+
+  /**
+   * @dev Emitted when create Lock with parameters (account, delegate, amount, slopePeriod, cliff)
+   */
+  event LockCreate(
+    uint256 indexed id,
+    address indexed account,
+    address indexed delegate,
+    uint256 time,
+    uint256 amount,
+    uint256 slopePeriod,
+    uint256 cliff
+  );
+  /**
+   * @dev Emitted when change Lock parameters (newDelegate, newAmount, newSlopePeriod, newCliff) for Lock with given id
+   */
+  event Relock(
+    uint256 indexed id,
+    address indexed account,
+    address indexed delegate,
+    uint256 counter,
+    uint256 time,
+    uint256 amount,
+    uint256 slopePeriod,
+    uint256 cliff
+  );
+  /**
+   * @dev Emitted when to set newDelegate address for Lock with given id
+   */
+  event Delegate(uint256 indexed id, address indexed account, address indexed delegate, uint256 time);
+  /**
+   * @dev Emitted when withdraw amount of Rari, account - msg.sender, amount - amount Rari
+   */
+  event Withdraw(address indexed account, uint256 amount);
+  /**
+   * @dev Emitted when migrate Locks with given id, account - msg.sender
+   */
+  event Migrate(address indexed account, uint256[] id);
+  /**
+   * @dev Stop run contract functions, accept withdraw, account - msg.sender
+   */
+  event StopLocking(address indexed account);
+  /**
+   * @dev Start run contract functions, accept withdraw, account - msg.sender
+   */
+  event StartLocking(address indexed account);
+  /**
+   * @dev StartMigration initiate migration to another contract, account - msg.sender, to - address delegate to
+   */
+  event StartMigration(address indexed account, address indexed to);
+  /**
+   * @dev set newMinCliffPeriod
+   */
+  event SetMinCliffPeriod(uint256 indexed newMinCliffPeriod);
+  /**
+   * @dev set newMinSlopePeriod
+   */
+  event SetMinSlopePeriod(uint256 indexed newMinSlopePeriod);
+  /**
+   * @dev set startingPointWeek
+   */
+  event SetStartingPointWeek(uint256 indexed newStartingPointWeek);
+
+  function __LockingBase_init_unchained(
+    IERC20Upgradeable _token,
+    uint32 _startingPointWeek,
+    uint32 _minCliffPeriod,
+    uint32 _minSlopePeriod
+  ) internal onlyInitializing {
+    token = _token;
+    startingPointWeek = _startingPointWeek;
+
+    //setting min cliff and slope
+    require(_minCliffPeriod <= MAX_CLIFF_PERIOD, "cliff too big");
+    require(_minSlopePeriod <= MAX_SLOPE_PERIOD, "period too big");
+    minCliffPeriod = _minCliffPeriod;
+    minSlopePeriod = _minSlopePeriod;
+  }
+
+  function addLines(
+    address account,
+    address _delegate,
+    uint96 amount,
+    uint32 slopePeriod,
+    uint32 cliff,
+    uint32 time,
+    uint32 currentBlock
+  ) internal {
+    require(slopePeriod <= amount, "Wrong value slopePeriod");
+    updateLines(account, _delegate, time);
+    (uint96 stAmount, uint96 stSlope) = getLock(amount, slopePeriod, cliff);
+    LibBrokenLine.Line memory line = LibBrokenLine.Line(time, stAmount, stSlope, cliff);
+    totalSupplyLine.addOneLine(counter, line, currentBlock);
+    accounts[_delegate].balance.addOneLine(counter, line, currentBlock);
+    {
+      uint96 slope = divUp(amount, slopePeriod);
+      line = LibBrokenLine.Line(time, amount, slope, cliff);
+    }
+    accounts[account].locked.addOneLine(counter, line, currentBlock);
+    locks[counter].account = account;
+    locks[counter].delegate = _delegate;
+  }
+
+  function updateLines(
+    address account,
+    address _delegate,
+    uint32 time
+  ) internal {
+    totalSupplyLine.update(time);
+    accounts[_delegate].balance.update(time);
+    accounts[account].locked.update(time);
+  }
+
+  /**
+   * Сalculate and return (newAmount, newSlope), using formula:
+   * locking = (tokens * (
+   *      ST_FORMULA_CONST_MULTIPLIER
+   *      + ST_FORMULA_CLIFF_MULTIPLIER * (cliffPeriod - minCliffPeriod))/(MAX_CLIFF_PERIOD - minCliffPeriod)
+   *      + ST_FORMULA_SLOPE_MULTIPLIER * (slopePeriod - minSlopePeriod))/(MAX_SLOPE_PERIOD - minSlopePeriod)
+   *      )) / ST_FORMULA_DIVIDER
+   **/
+  function getLock(
+    uint96 amount,
+    uint32 slopePeriod,
+    uint32 cliff
+  ) public view returns (uint96 lockAmount, uint96 lockSlope) {
+    require(cliff >= minCliffPeriod, "cliff period < minimal lock period");
+    require(slopePeriod >= minSlopePeriod, "slope period < minimal lock period");
+
+    uint96 cliffSide = (uint96(cliff - uint32(minCliffPeriod)) * (ST_FORMULA_CLIFF_MULTIPLIER)) /
+      (MAX_CLIFF_PERIOD - uint32(minCliffPeriod));
+    uint96 slopeSide = (uint96((slopePeriod - uint32(minSlopePeriod))) * (ST_FORMULA_SLOPE_MULTIPLIER)) /
+      (MAX_SLOPE_PERIOD - uint32(minSlopePeriod));
+    uint96 multiplier = cliffSide + (slopeSide) + (ST_FORMULA_CONST_MULTIPLIER);
+
+    uint256 amountMultiplied = uint256(amount) * uint256(multiplier);
+    lockAmount = uint96(amountMultiplied / (ST_FORMULA_DIVIDER));
+    lockSlope = divUp(lockAmount, slopePeriod);
+  }
+
+  function divUp(uint96 a, uint96 b) internal pure returns (uint96) {
+    return ((a - 1) / b) + 1;
+  }
+
+  function roundTimestamp(uint32 ts) public view returns (uint32) {
+    if (ts < getEpochShift()) {
+      return 0;
+    }
+    uint32 shifted = ts - (getEpochShift());
+    return shifted / WEEK - uint32(startingPointWeek);
+  }
+
+  /**
+   * @notice method returns the amount of blocks to shift locking epoch to.
+   * By the time of development, the default weekly-epoch calculated by main-net block number
+   * would start at about 11-35 UTC on Tuesday
+   * we move it to 00-00 UTC Thursday by adding 10800 blocks (approx)
+   */
+  function getEpochShift() internal view virtual returns (uint32) {
+    return 10800;
+  }
+
+  function verifyLockOwner(uint256 id) internal view returns (address account) {
+    account = locks[id].account;
+    require(account == msg.sender, "caller not a lock owner");
+  }
+
+  function getBlockNumber() internal view virtual returns (uint32) {
+    return uint32(block.number);
+  }
+
+  function setStartingPointWeek(uint32 newStartingPointWeek) public notStopped notMigrating onlyOwner {
+    require(newStartingPointWeek < roundTimestamp(getBlockNumber()), "wrong newStartingPointWeek");
+    startingPointWeek = newStartingPointWeek;
+
+    emit SetStartingPointWeek(newStartingPointWeek);
+  }
+
+  function setMinCliffPeriod(uint32 newMinCliffPeriod) external notStopped notMigrating onlyOwner {
+    require(newMinCliffPeriod < MAX_CLIFF_PERIOD, "new cliff period > 2 years");
+    minCliffPeriod = newMinCliffPeriod;
+
+    emit SetMinCliffPeriod(newMinCliffPeriod);
+  }
+
+  function setMinSlopePeriod(uint32 newMinSlopePeriod) external notStopped notMigrating onlyOwner {
+    require(newMinSlopePeriod < MAX_SLOPE_PERIOD, "new slope period > 2 years");
+    minSlopePeriod = newMinSlopePeriod;
+
+    emit SetMinSlopePeriod(newMinSlopePeriod);
+  }
+
+  /**
+        @notice checks if the line is relevant and needs to be copied to the new data structure
+     */
+  function isRelevant(uint256 id)
+    external
+    view
+    returns (
+      bool,
+      uint256,
+      address,
+      uint256,
+      address
+    )
+  {
+    uint32 currentBlock = getBlockNumber();
+    uint32 currentEpoch = roundTimestamp(currentBlock);
+
+    address delegate = locks[id].delegate;
+    LibBrokenLine.LineDataOld storage oldLineBalance = accountsOld[delegate].balance.initiatedLines[id];
+
+    address account = locks[id].account;
+    LibBrokenLine.LineDataOld storage oldLineLocked = accountsOld[account].locked.initiatedLines[id];
+
+    //line adds at time start + cliff + slopePeriod + 1(mod)
+    uint256 slopeLocked = (oldLineLocked.line.bias / oldLineLocked.line.slope);
+    uint256 slopeBalance = (oldLineBalance.line.bias / oldLineBalance.line.slope);
+    uint256 slope = slopeLocked > slopeBalance ? slopeLocked : slopeBalance;
+
+    uint256 finishTime = oldLineLocked.line.start + oldLineLocked.cliff + slope + 1;
+
+    return (
+      (finishTime < currentEpoch) ? false : true,
+      oldLineBalance.line.start,
+      delegate,
+      oldLineLocked.line.start,
+      account
+    );
+  }
+
+  /**
+   * @dev Throws if stopped
+   */
+  modifier notStopped() {
+    require(!stopped, "stopped");
+    _;
+  }
+
+  /**
+   * @dev Throws if not stopped
+   */
+  modifier isStopped() {
+    require(stopped, "not stopped");
+    _;
+  }
+
+  modifier notMigrating() {
+    require(migrateTo == address(0), "migrating");
+    _;
+  }
+
+  function updateAccountLines(address account, uint32 time) public notStopped notMigrating onlyOwner {
+    accounts[account].balance.update(time);
+    accounts[account].locked.update(time);
+  }
+
+  function updateTotalSupplyLine(uint32 time) public notStopped notMigrating onlyOwner {
+    totalSupplyLine.update(time);
+  }
+
+  function updateAccountLinesBlockNumber(address account, uint32 blockNumber)
+    external
+    notStopped
+    notMigrating
+    onlyOwner
+  {
+    uint32 time = roundTimestamp(blockNumber);
+    updateAccountLines(account, time);
+  }
+
+  function updateTotalSupplyLineBlockNumber(uint32 blockNumber) external notStopped notMigrating onlyOwner {
+    uint32 time = roundTimestamp(blockNumber);
+    updateTotalSupplyLine(time);
+  }
+
+  function migrateBalanceLines(uint256[] calldata ids) external onlyOwner {
+    uint256 len = ids.length;
+    for (uint256 i = 0; i < len; i++) {
+      uint256 id = ids[i];
+      Lock storage lock = locks[id];
+      address user = lock.delegate;
+      LibBrokenLine.LineDataOld storage oldLine = accountsOld[user].balance.initiatedLines[id];
+
+      LibBrokenLine.Line memory line = LibBrokenLine.Line({
+        start: uint32(oldLine.line.start),
+        bias: uint96(oldLine.line.bias),
+        slope: uint96(oldLine.line.slope),
+        cliff: uint32(oldLine.cliff)
+      });
+
+      //adding the line to balance broken line
+      accounts[user].balance._addOneLine(id, line);
+      //adding the line to totalSupply broken line
+      totalSupplyLine._addOneLine(id, line);
+    }
+  }
+
+  function migrateLockedLines(uint256[] calldata ids) external onlyOwner {
+    uint256 len = ids.length;
+    for (uint256 i = 0; i < len; i++) {
+      uint256 id = ids[i];
+      Lock storage lock = locks[id];
+      address user = lock.account;
+      LibBrokenLine.LineDataOld storage oldLine = accountsOld[user].locked.initiatedLines[id];
+
+      LibBrokenLine.Line memory line = LibBrokenLine.Line({
+        start: uint32(oldLine.line.start),
+        bias: uint96(oldLine.line.bias),
+        slope: uint96(oldLine.line.slope),
+        cliff: uint32(oldLine.cliff)
+      });
+
+      //adding the line to balance broken line
+      accounts[user].locked._addOneLine(id, line);
+    }
+  }
+
+  function copyAmountMakeSnapshots(address[] calldata users) external onlyOwner {
+    uint32 currentBlock = getBlockNumber();
+    uint32 currentEpoch = roundTimestamp(currentBlock);
+    uint256 len = users.length;
+    for (uint256 i = 0; i < len; i++) {
+      Account storage newData = accounts[users[i]];
+      AccountOld storage oldData = accountsOld[users[i]];
+
+      //copy amount
+      newData.amount = uint96(oldData.amount);
+
+      if (newData.balance.initial.bias > 0) {
+        newData.balance.update(currentEpoch);
+        newData.balance.saveSnapshot(currentEpoch, currentBlock);
+      }
+
+      if (newData.locked.initial.bias > 0) {
+        newData.locked.update(currentEpoch);
+        newData.locked.saveSnapshot(currentEpoch, currentBlock);
+      }
+    }
+
+    totalSupplyLine.saveSnapshot(currentEpoch, currentBlock);
+  }
+
+  //48 => 43 add new accounts and totalSupplyLine
+  uint256[43] private __gap;
+}
---
+++ contracts/governance/libs/LibBrokenLine.sol
@@ -0,0 +1,324 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "./LibIntMapping.sol";
+
+/**
+ * Line describes a linear function, how the user's voice decreases from point (start, bias) with speed slope
+ * BrokenLine - a curve that describes the curve of the change in the sum of votes of several users
+ * This curve starts with a line (Line) and then, at any time, the slope can be changed.
+ * All slope changes are stored in slopeChanges. The slope can always be reduced only, it cannot increase,
+ * because users can only run out of lockup periods.
+ **/
+
+library LibBrokenLine {
+  using LibIntMapping for mapping(uint256 => int96);
+
+  // OldStructs
+  struct LineOld {
+    uint256 start;
+    uint256 bias;
+    uint256 slope;
+  }
+
+  struct LineDataOld {
+    //all data about line
+    LineOld line;
+    uint256 cliff;
+  }
+
+  struct BrokenLineOld {
+    mapping(uint256 => int256) slopeChanges; //change of slope applies to the next time point
+    mapping(uint256 => int256) biasChanges; //change of bias applies to the next time point
+    mapping(uint256 => LineDataOld) initiatedLines; //initiated (successfully added) Lines
+    LineOld initial;
+  }
+  // end of the old structs
+
+  struct Line {
+    uint32 start;
+    uint96 bias;
+    uint96 slope;
+    uint32 cliff;
+  }
+
+  struct Point {
+    uint32 blockNumber;
+    uint96 bias;
+    uint96 slope;
+    uint32 epoch;
+  }
+
+  struct BrokenLine {
+    mapping(uint256 => int96) slopeChanges; //change of slope applies to the next time point
+    mapping(uint256 => Line) initiatedLines; //initiated (successfully added) Lines
+    Point[] history;
+    Line initial;
+  }
+
+  /**
+   * @dev Add Line, save data in LineData. Run update BrokenLine, require:
+   *      1. slope != 0, slope <= bias
+   *      2. line not exists
+   **/
+  function _addOneLine(
+    BrokenLine storage brokenLine,
+    uint256 id,
+    Line memory line
+  ) internal {
+    require(line.slope != 0, "Slope == 0, unacceptable value for slope");
+    require(line.slope <= line.bias, "Slope > bias, unacceptable value for slope");
+    require(brokenLine.initiatedLines[id].bias == 0, "Line with given id is already exist");
+    brokenLine.initiatedLines[id] = line;
+
+    update(brokenLine, line.start);
+    brokenLine.initial.bias = brokenLine.initial.bias + (line.bias);
+    //save bias for history in line.start minus one
+    uint32 lineStartMinusOne = line.start - 1;
+    //period is time without tail
+    uint32 period = uint32(line.bias / (line.slope));
+
+    if (line.cliff == 0) {
+      //no cliff, need to increase brokenLine.initial.slope write now
+      brokenLine.initial.slope = brokenLine.initial.slope + (line.slope);
+      //no cliff, save slope in history in time minus one
+      brokenLine.slopeChanges.addToItem(lineStartMinusOne, safeInt(line.slope));
+    } else {
+      //cliffEnd finish in lineStart minus one plus cliff
+      uint32 cliffEnd = lineStartMinusOne + (line.cliff);
+      //save slope in history in cliffEnd
+      brokenLine.slopeChanges.addToItem(cliffEnd, safeInt(line.slope));
+      period = period + (line.cliff);
+    }
+
+    int96 mod = safeInt(line.bias % (line.slope));
+    uint32 endPeriod = line.start + (period);
+    uint32 endPeriodMinus1 = endPeriod - 1;
+    brokenLine.slopeChanges.subFromItem(endPeriodMinus1, safeInt(line.slope) - (mod));
+    brokenLine.slopeChanges.subFromItem(endPeriod, mod);
+  }
+
+  /**
+   * @dev adding a line and saving snapshot
+   */
+  function addOneLine(
+    BrokenLine storage brokenLine,
+    uint256 id,
+    Line memory line,
+    uint32 blockNumber
+  ) internal {
+    _addOneLine(brokenLine, id, line);
+    saveSnapshot(brokenLine, line.start, blockNumber);
+  }
+
+  /**
+   * @dev Remove Line from BrokenLine, return bias, slope, cliff. Run update BrokenLine.
+   **/
+  function _remove(
+    BrokenLine storage brokenLine,
+    uint256 id,
+    uint32 toTime
+  )
+    internal
+    returns (
+      uint96 bias,
+      uint96 slope,
+      uint32 cliff
+    )
+  {
+    Line memory line = brokenLine.initiatedLines[id];
+    require(line.bias != 0, "Removing Line, which not exists");
+
+    update(brokenLine, toTime);
+    //check time Line is over
+    bias = line.bias;
+    slope = line.slope;
+    cliff = 0;
+    //for information: bias / (slope) - this`s period while slope works
+    uint32 finishTime = line.start + (uint32(bias / (slope))) + (line.cliff);
+    if (toTime > finishTime) {
+      bias = 0;
+      slope = 0;
+      return (bias, slope, cliff);
+    }
+    uint32 finishTimeMinusOne = finishTime - 1;
+    uint32 toTimeMinusOne = toTime - 1;
+    int96 mod = safeInt(bias % slope);
+    uint32 cliffEnd = line.start + (line.cliff) - 1;
+    if (toTime <= cliffEnd) {
+      //cliff works
+      cliff = cliffEnd - (toTime) + 1;
+      //in cliff finish time compensate change slope by oldLine.slope
+      brokenLine.slopeChanges.subFromItem(cliffEnd, safeInt(slope));
+      //in new Line finish point use oldLine.slope
+      brokenLine.slopeChanges.addToItem(finishTimeMinusOne, safeInt(slope) - (mod));
+    } else if (toTime <= finishTimeMinusOne) {
+      //slope works
+      //now compensate change slope by oldLine.slope
+      brokenLine.initial.slope = brokenLine.initial.slope - (slope);
+      //in new Line finish point use oldLine.slope
+      brokenLine.slopeChanges.addToItem(finishTimeMinusOne, safeInt(slope) - (mod));
+      bias = (uint96(finishTime - (toTime)) * slope) + (uint96(mod));
+      //save slope for history
+      brokenLine.slopeChanges.subFromItem(toTimeMinusOne, safeInt(slope));
+    } else {
+      //tail works
+      //now compensate change slope by tail
+      brokenLine.initial.slope = brokenLine.initial.slope - (uint96(mod));
+      bias = uint96(mod);
+      slope = bias;
+      //save slope for history
+      brokenLine.slopeChanges.subFromItem(toTimeMinusOne, safeInt(slope));
+    }
+    brokenLine.slopeChanges.addToItem(finishTime, mod);
+    brokenLine.initial.bias = brokenLine.initial.bias - (bias);
+    brokenLine.initiatedLines[id].bias = 0;
+  }
+
+  /**
+   * @dev removing a line and saving snapshot
+   */
+  function remove(
+    BrokenLine storage brokenLine,
+    uint256 id,
+    uint32 toTime,
+    uint32 blockNumber
+  )
+    internal
+    returns (
+      uint96 bias,
+      uint96 slope,
+      uint32 cliff
+    )
+  {
+    (bias, slope, cliff) = _remove(brokenLine, id, toTime);
+    saveSnapshot(brokenLine, toTime, blockNumber);
+  }
+
+  /**
+   * @dev Update initial Line by parameter toTime. Calculate and set all changes
+   **/
+  function update(BrokenLine storage brokenLine, uint32 toTime) internal {
+    uint32 time = brokenLine.initial.start;
+    if (time == toTime) {
+      return;
+    }
+    uint96 slope = brokenLine.initial.slope;
+    uint96 bias = brokenLine.initial.bias;
+    if (bias != 0) {
+      require(toTime > time, "can't update BrokenLine for past time");
+      while (time < toTime) {
+        bias = bias - (slope);
+
+        int96 newSlope = safeInt(slope) + (brokenLine.slopeChanges[time]);
+        require(newSlope >= 0, "slope < 0, something wrong with slope");
+        slope = uint96(newSlope);
+
+        time = time + 1;
+      }
+    }
+    brokenLine.initial.start = toTime;
+    brokenLine.initial.bias = bias;
+    brokenLine.initial.slope = slope;
+  }
+
+  function actualValue(
+    BrokenLine storage brokenLine,
+    uint32 toTime,
+    uint32 toBlock
+  ) internal view returns (uint96) {
+    uint32 fromTime = brokenLine.initial.start;
+    if (fromTime == toTime) {
+      if (brokenLine.history[brokenLine.history.length - 1].blockNumber < toBlock) {
+        return (brokenLine.initial.bias);
+      } else {
+        return actualValueBack(brokenLine, toTime, toBlock);
+      }
+    }
+    if (toTime > fromTime) {
+      return
+        actualValueForward(brokenLine, fromTime, toTime, brokenLine.initial.bias, brokenLine.initial.slope, toBlock);
+    }
+    return actualValueBack(brokenLine, toTime, toBlock);
+  }
+
+  function actualValueForward(
+    BrokenLine storage brokenLine,
+    uint32 fromTime,
+    uint32 toTime,
+    uint96 bias,
+    uint96 slope,
+    uint32 toBlock
+  ) internal view returns (uint96) {
+    if ((bias == 0)) {
+      return (bias);
+    }
+    uint32 time = fromTime;
+
+    while (time < toTime) {
+      bias = bias - (slope);
+
+      int96 newSlope = safeInt(slope) + (brokenLine.slopeChanges[time]);
+      require(newSlope >= 0, "slope < 0, something wrong with slope");
+      slope = uint96(newSlope);
+
+      time = time + 1;
+    }
+    return bias;
+  }
+
+  function actualValueBack(
+    BrokenLine storage brokenLine,
+    uint32 toTime,
+    uint32 toBlock
+  ) internal view returns (uint96) {
+    (uint96 bias, uint96 slope, uint32 fromTime) = binarySearch(brokenLine.history, toBlock);
+    return actualValueForward(brokenLine, fromTime, toTime, bias, slope, toBlock);
+  }
+
+  function safeInt(uint96 value) internal pure returns (int96 result) {
+    require(value < 2**95, "int cast error");
+    result = int96(value);
+  }
+
+  function saveSnapshot(
+    BrokenLine storage brokenLine,
+    uint32 epoch,
+    uint32 blockNumber
+  ) internal {
+    brokenLine.history.push(
+      Point({ blockNumber: blockNumber, bias: brokenLine.initial.bias, slope: brokenLine.initial.slope, epoch: epoch })
+    );
+  }
+
+  function binarySearch(Point[] memory history, uint32 toBlock)
+    internal
+    pure
+    returns (
+      uint96,
+      uint96,
+      uint32
+    )
+  {
+    uint256 len = history.length;
+    if (len == 0 || history[0].blockNumber > toBlock) {
+      return (0, 0, 0);
+    }
+    uint256 min = 0;
+    uint256 max = len - 1;
+
+    for (uint256 i = 0; i < 128; i++) {
+      if (min >= max) {
+        break;
+      }
+      uint256 mid = (min + max + 1) / 2;
+      if (history[mid].blockNumber <= toBlock) {
+        min = mid;
+      } else {
+        max = mid - 1;
+      }
+    }
+    return (history[min].bias, history[min].slope, history[min].epoch);
+  }
+}
