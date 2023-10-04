42c5658fe927ce15487eb8a89f0a4eb8eb99e617
baroooo <baranseltekin@gmail.com>
2023-10-04 14:08:17 +0200

    fix: consistent licencing

--- test/mocks/MockMentoToken.sol
+++ test/mocks/MockMentoToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 import { ERC20 } from "openzeppelin-contracts-next/contracts/token/ERC20/ERC20.sol";
