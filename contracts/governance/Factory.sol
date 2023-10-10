85a8e15dbf1a3ed7bd68b96e002864e2000715e8
baroooo <baranseltekin@gmail.com>
2023-10-10 18:50:52 +0200

    chore: solhint

--- contracts/governance/Factory.sol
+++ contracts/governance/Factory.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
+// solhint-disable max-line-length
 
 import { MentoToken } from "./MentoToken.sol";
 import { Emission } from "./Emission.sol";
