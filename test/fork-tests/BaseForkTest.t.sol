c4a6a4e55cf4c9ceb2a993521d9a3d4c005dab0e
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2023-11-16 15:43:04 +0100

    fix: mint more collateral to the reserve

--- test/fork-tests/BaseForkTest.t.sol
+++ test/fork-tests/BaseForkTest.t.sol
@@ -128,8 +128,8 @@ contract BaseForkTest is Test, TokenHelpers, TestAsserts {
     // XXX: The number of collateral assets 3 is hardcoded here [CELO, USDC, EUROC]
     for (uint256 i = 0; i < 3; i++) {
       address collateralAsset = reserve.collateralAssets(i);
-      mint(collateralAsset, address(reserve), Utils.toSubunits(10_000_000, collateralAsset));
-      console.log("Minting 10mil %s to reserve", IERC20Metadata(collateralAsset).symbol());
+      mint(collateralAsset, address(reserve), Utils.toSubunits(25_000_000, collateralAsset));
+      console.log("Minting 25mil %s to reserve", IERC20Metadata(collateralAsset).symbol());
     }
 
     console.log("Exchanges(%d): ", exchanges.length);
