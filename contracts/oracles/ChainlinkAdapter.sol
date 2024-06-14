ec936f0ed0f803852e7af29d1b3e065bde572597
Marcin Chrzanowski <martin@clabs.co>
2024-06-14 16:47:54 +0200

    docs: add NatSpec documentation comments

--- contracts/oracles/ChainlinkAdapter.sol
+++ contracts/oracles/ChainlinkAdapter.sol
@@ -25,6 +25,11 @@ interface ISortedOraclesMin {
  * feed.
  */
 contract ChainlinkAdapter is IChainlinkAdapter {
+    /**
+     * @notice The number of digits after the decimal point in FixidityLib
+     * values, as used by SortedOracles.
+     * @dev See contracts/common/FixidityLib.sol
+     */
     uint256 constant public FIXIDITY_DECIMALS = 24;
     /// @notice The rateFeedId this adapter relays for.
     address immutable public token;
@@ -36,10 +41,28 @@ contract ChainlinkAdapter is IChainlinkAdapter {
      */
     address immutable public aggregator;
 
+    /**
+     * @notice Used when a new answer's timestamp is not newer than the most recent
+     * SortedOracles timestamp.
+     */
     error OldTimestamp();
+    /**
+     * @notice Used when a new answer's timestamp would be considered expired by
+     * SortedOracles.
+     */
     error ExpiredTimestamp();
+    /**
+     * @notice Used when a negative answer is returned by the Chainlink
+     * aggregator.
+     */
     error NegativeAnswer();
 
+    /**
+     * @notice Initializes the contract, setting immutable parameters.
+     * @param _token Address of the rate feed this adapter relays for.
+     * @param _sortedOracles Address of the SortedOracles contract to relay to.
+     * @param _aggregator Address of the Chainlink aggregator to fetch data from.
+     */
     constructor(address _token, address _sortedOracles, address _aggregator) {
         token = _token;
         sortedOracles = _sortedOracles;
@@ -83,10 +106,21 @@ contract ChainlinkAdapter is IChainlinkAdapter {
         ISortedOraclesMin(sortedOracles).report(token, report, address(0), address(0));
     }
 
+    /**
+     * @notice Checks if a Chainlink answer's timestamp would be expired in
+     * SortedOracles.
+     * @param timestamp The timestamp returned by the Chainlink aggregator.
+     * @return `true` if expired based on SortedOracles expiry parameter.
+     */
     function isTimestampExpired(uint256 timestamp) internal view returns (bool) {
         return block.timestamp - timestamp >= ISortedOraclesMin(sortedOracles).getTokenReportExpirySeconds(token);
     }
 
+    /**
+     * @notice Converts a Chainlink answer to an unwrapped Fixidity value.
+     * @param answer An answer from the Chainlink aggregator.
+     * @return The converted Fixidity value (with 24 decimals).
+     */
     function chainlinkToFixidity(int256 answer) internal view returns (uint256) {
         uint256 chainlinkDecimals = uint256(AggregatorV3Interface(aggregator).decimals());
         return uint256(answer) * 10 ** (FIXIDITY_DECIMALS - chainlinkDecimals);
