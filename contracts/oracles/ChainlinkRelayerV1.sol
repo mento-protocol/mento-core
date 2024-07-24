b1aa4bd64aa84b4a2ddd83cfd2c6f810e0684ce8
Philip Paetz <philip.paetz@me.com>
2024-07-24 12:58:04 +0200

    chore(ChainlinkRelayer): update relayer contracts from 0.8.18 to 0.8.20

--- contracts/oracles/ChainlinkRelayerV1.sol
+++ contracts/oracles/ChainlinkRelayerV1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import "../interfaces/IChainlinkRelayer.sol";
 import "foundry-chainlink-toolkit/src/interfaces/feeds/AggregatorV3Interface.sol";
