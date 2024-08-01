4eb9625b827c34cd2cb8457909b3759bca1020db
Marcin Chrzanowski <martin@clabs.co>
2024-08-01 22:55:36 +0200

    fix: add function to factory interface

--- contracts/interfaces/IChainlinkRelayerFactory.sol
+++ contracts/interfaces/IChainlinkRelayerFactory.sol
@@ -15,4 +15,6 @@ interface IChainlinkRelayerFactory {
   function getRelayer(address rateFeedId) external view returns (address);
 
   function getRelayers() external view returns (address[] memory);
+
+  function computedRelayerAddress(address rateFeedId, address chainlinkAggregator) external returns (address);
 }
