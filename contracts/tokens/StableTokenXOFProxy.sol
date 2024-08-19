de60af3054325107c6c0b90a69bf34b444de6478
bowd <dumbogdan@gmail.com>
2024-08-19 20:28:07 +0200

    feat: move proxies around

--- contracts/tokens/StableTokenXOFProxy.sol
+++ contracts/tokens/StableTokenXOFProxy.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.5.13;
 
-import "../../common/Proxy.sol";
+import "../common/Proxy.sol";
 
 /* solhint-disable-next-line no-empty-blocks */
-contract StableTokenCOPProxy is Proxy {
+contract StableTokenXOFProxy is Proxy {
 
 }
