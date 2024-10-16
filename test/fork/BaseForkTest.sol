0da8564b6b9f8827cc375a9380dd46200fb39d41
Philip Paetz <philip.paetz@me.com>
2024-10-16 17:57:12 +0200

    chore: (almost) fix fork tests after new gooddollar & PUSO changes

--- test/fork/BaseForkTest.sol
+++ test/fork/BaseForkTest.sol
@@ -2,17 +2,28 @@
 // solhint-disable func-name-mixedcase, var-name-mixedcase, state-visibility, const-name-snakecase, max-states-count
 pragma solidity ^0.8;
 
+// Libraries
 import { Test } from "mento-std/Test.sol";
 import { CELO_REGISTRY_ADDRESS } from "mento-std/Constants.sol";
-
 import { FixidityLib } from "celo/contracts/common/FixidityLib.sol";
-import { IRegistry } from "celo/contracts/common/interfaces/IRegistry.sol";
 
+// Interfaces
+import { IBiPoolManager } from "contracts/interfaces/IBiPoolManager.sol";
 import { IBreakerBox } from "contracts/interfaces/IBreakerBox.sol";
-import { Broker } from "contracts/swap/Broker.sol";
+import { IOwnable } from "contracts/interfaces/IOwnable.sol";
+import { IPricingModule } from "contracts/interfaces/IPricingModule.sol";
+import { IRegistry } from "celo/contracts/common/interfaces/IRegistry.sol";
 import { IReserve } from "contracts/interfaces/IReserve.sol";
 import { ISortedOracles } from "contracts/interfaces/ISortedOracles.sol";
+import { IStableTokenV2 } from "contracts/interfaces/IStableTokenV2.sol";
+import { ITradingLimits } from "contracts/interfaces/ITradingLimits.sol";
 import { ITradingLimitsHarness } from "test/utils/harnesses/ITradingLimitsHarness.sol";
+
+// Contracts & Utils
+import { Broker } from "contracts/swap/Broker.sol";
+import { TradingLimitsHarness } from "test/utils/harnesses/TradingLimitsHarness.sol";
+import { TestERC20 } from "test/utils/mocks/TestERC20.sol";
+import { USDC } from "test/utils/mocks/USDC.sol";
 import { toRateFeed } from "./helpers/misc.sol";
 
 interface IMint {
@@ -36,6 +47,7 @@ abstract contract BaseForkTest is Test {
 
   address governance;
   Broker public broker;
+  IBiPoolManager biPoolManager;
   IBreakerBox public breakerBox;
   ISortedOracles public sortedOracles;
   IReserve public reserve;
@@ -48,6 +60,16 @@ abstract contract BaseForkTest is Test {
   uint8 public constant COLLATERAL_ASSETS_COUNT = 5;
 
   uint256 targetChainId;
+
+  // TODO: Should use real USDC here
+  TestERC20 usdcToken;
+
+  // TODO: Should use real EUROC here
+  TestERC20 eurocToken;
+  IStableTokenV2 cUSDToken;
+  IStableTokenV2 cEURToken;
+  IStableTokenV2 eXOFToken;
+  IStableTokenV2 cCOPToken;
 
   constructor(uint256 _targetChainId) Test() {
     targetChainId = _targetChainId;
@@ -71,14 +93,22 @@ abstract contract BaseForkTest is Test {
     // The precompile handler needs to be reinitialized after forking.
     __CeloPrecompiles_init();
 
-    tradingLimits = ITradingLimitsHarness(deployCode("TradingLimitsHarness"));
-    broker = Broker(lookup("Broker"));
+    tradingLimits = new TradingLimitsHarness();
+
+    // TODO: Replace with `lookup("Broker")` after we've updated the broker on mainnet
+    broker = new Broker(true);
+
+    // TODO: Probably can't do it like this to be compatible between both Alfajores & Mainnet, but couldn't find BiPoolManager in registry?
+    biPoolManager = IBiPoolManager(0x22d9db95E6Ae61c104A7B6F6C78D7993B94ec901);
     sortedOracles = ISortedOracles(lookup("SortedOracles"));
     governance = lookup("Governance");
     breakerBox = IBreakerBox(address(sortedOracles.breakerBox()));
     vm.label(address(breakerBox), "BreakerBox");
     trader = makeAddr("trader");
     reserve = IReserve(lookup("Reserve"));
+
+    setUpAssets();
+    setUpBroker();
 
     /// @dev Hardcoded number of dependencies for each ratefeed.
     /// Should be updated when they change, there is a test that will
@@ -87,6 +117,78 @@ abstract contract BaseForkTest is Test {
     rateFeedDependenciesCount[toRateFeed("EUROCXOF")] = 2;
     rateFeedDependenciesCount[toRateFeed("USDCEUR")] = 1;
     rateFeedDependenciesCount[toRateFeed("USDCBRL")] = 1;
+  }
+
+  function setUpAssets() internal {
+    usdcToken = new USDC("bridgedUSDC", "bridgedUSDC");
+    eurocToken = new USDC("bridgedEUROC", "bridgedEUROC");
+
+    // TODO: Probably can't do it like this to be compatible between both Alfajores & Mainnet, but couldn't find it in the registry?
+    cUSDToken = IStableTokenV2(0x765DE816845861e75A25fCA122bb6898B8B1282a);
+    vm.startPrank(IOwnable(address(cUSDToken)).owner());
+    cUSDToken.setBroker(address(broker));
+
+    // TODO: Probably can't do it like this to be compatible between both Alfajores & Mainnet, but couldn't find it in the registry?
+    cEURToken = IStableTokenV2(0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73);
+    cEURToken.setBroker(address(broker));
+
+    // TODO: Probably can't do it like this to be compatible between both Alfajores & Mainnet, but couldn't find it in the registry?
+    eXOFToken = IStableTokenV2(0x73F93dcc49cB8A239e2032663e9475dd5ef29A08);
+    eXOFToken.setBroker(address(broker));
+
+    // TODO: Probably can't do it like this to be compatible between both Alfajores & Mainnet, but couldn't find it in the registry?
+    // TODO: Do we use cCOP or PUSO?
+    cCOPToken = IStableTokenV2(0x8A567e2aE79CA692Bd748aB832081C45de4041eA);
+    vm.stopPrank();
+
+    vm.label(address(cUSDToken), "cUSD");
+    vm.label(address(cEURToken), "cEUR");
+    vm.label(address(eXOFToken), "eXOF");
+  }
+
+  function setUpBroker() internal {
+    vm.prank(biPoolManager.owner());
+    biPoolManager.setBroker(address(broker));
+
+    address[] memory exchangeProviders = new address[](1);
+    exchangeProviders[0] = address(biPoolManager);
+
+    address[] memory reserves = new address[](1);
+    reserves[0] = address(reserve);
+
+    broker.initialize(exchangeProviders, reserves);
+
+    vm.prank(IOwnable(address(registry)).owner());
+    registry.setAddressFor("Broker", address(broker));
+
+    vm.prank(reserve.owner());
+    reserve.addExchangeSpender(address(broker));
+  }
+
+  function setUp_tradingLimits() internal {
+    ITradingLimits.Config memory config = configL0L1LG(100, 10000, 1000, 100000, 1000000);
+
+    bytes32 pair_cUSD_CELO_ID = keccak256(abi.encodePacked("cUSD", "CELO", "ConstantProduct"));
+    broker.configureTradingLimit(pair_cUSD_CELO_ID, address(cUSDToken), config);
+
+    bytes32 pair_cEUR_CELO_ID = keccak256(abi.encodePacked("cEUR", "CELO", "ConstantProduct"));
+    broker.configureTradingLimit(pair_cEUR_CELO_ID, address(cEURToken), config);
+
+    bytes32 pair_cUSD_bridgedUSDC_ID = keccak256(abi.encodePacked("cUSD", "USDC", "ConstantSum"));
+    broker.configureTradingLimit(pair_cUSD_bridgedUSDC_ID, address(usdcToken), config);
+
+    bytes32 pair_cEUR_bridgedUSDC_ID = keccak256(abi.encodePacked("cEUR", "USDC", "ConstantProduct"));
+    broker.configureTradingLimit(pair_cEUR_bridgedUSDC_ID, address(usdcToken), config);
+
+    bytes32 pair_cUSD_cEUR_ID = keccak256(abi.encodePacked("cUSD", "cEUR", "ConstantProduct"));
+    broker.configureTradingLimit(pair_cUSD_cEUR_ID, address(cUSDToken), config);
+
+    bytes32 pair_eXOF_bridgedEUROC_ID = keccak256(abi.encodePacked("eXOF", "cEUR", "ConstantSum"));
+    broker.configureTradingLimit(pair_eXOF_bridgedEUROC_ID, address(eXOFToken), config);
+
+    // FIXME: This might be breaking the tests atm
+    bytes32 pair_cUSD_PUSO_ID = keccak256(abi.encodePacked("cUSD", "PUSO", "ConstantSum"));
+    broker.configureTradingLimit(pair_cUSD_PUSO_ID, address(cCOPToken), config);
   }
 
   function mint(address asset, address to, uint256 amount, bool updateSupply) public {
@@ -100,5 +202,20 @@ abstract contract BaseForkTest is Test {
     }
 
     deal(asset, to, amount, updateSupply);
+  }
+
+  function configL0L1LG(
+    uint32 timestep0,
+    int48 limit0,
+    uint32 timestep1,
+    int48 limit1,
+    int48 limitGlobal
+  ) internal pure returns (ITradingLimits.Config memory config) {
+    config.timestep0 = timestep0;
+    config.limit0 = limit0;
+    config.timestep1 = timestep1;
+    config.limit1 = limit1;
+    config.limitGlobal = limitGlobal;
+    config.flags = 1 | 2 | 4; //L0, L1, and LG
   }
 }
