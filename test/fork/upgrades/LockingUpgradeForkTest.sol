34cb12987ae07640a97dc61824ab0b624146b152
baroooo <baranseltekin@gmail.com>
2024-11-18 15:29:05 +0100

    chore: refactor fork test

--- test/fork/upgrades/LockingUpgradeForkTest.sol
+++ test/fork/upgrades/LockingUpgradeForkTest.sol
@@ -6,10 +6,9 @@ import { Locking } from "contracts/governance/locking/Locking.sol";
 import { GovernanceFactory } from "contracts/governance/GovernanceFactory.sol";
 import { ProxyAdmin } from "openzeppelin-contracts-next/contracts/proxy/transparent/ProxyAdmin.sol";
 import { ITransparentUpgradeableProxy } from "openzeppelin-contracts-next/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-import "forge-std/console.sol";
 
 // used to avoid stack too deep error
-struct Balances {
+struct LockingSnapshot {
   uint256 totalSupply;
   uint256 pastTotalSupply;
   uint256 balance1;
@@ -23,6 +22,7 @@ struct Balances {
   uint256 withdrawable1;
   uint256 withdrawable2;
 }
+
 contract LockingUpgradeForkTest is BaseForkTest {
   // airdrop claimers from the mainnet
   address public constant AIRDROP_CLAIMER_1 = 0x3152eE4a18ee3209524F9071A6BcAdA098f19838;
@@ -50,129 +50,141 @@ contract LockingUpgradeForkTest is BaseForkTest {
     newLockingImplementation = address(new Locking());
     vm.prank(timelockController);
     proxyAdmin.upgrade(ITransparentUpgradeableProxy(address(locking)), newLockingImplementation);
-  }
-
-  function test_upgrade() public {
-    Balances memory beforeBalances;
-    Balances memory afterBalances;
 
     vm.prank(timelockController);
     locking.setMentoLabsMultisig(mentoLabsMultisig);
 
-    // THU Nov-07-2024 12:00:23 AM +UTC
+    // THU Nov-07-2024 00:00:23 +UTC
     vm.roll(28653031);
     vm.warp(1730937623);
+  }
+
+  function test_blockNoDependentCalculations_afterL2Transition_shouldWorkAsBefore() public {
+    LockingSnapshot memory beforeSnapshot;
+    LockingSnapshot memory afterSnapshot;
 
     // move 3 weeks forward on L1
-    moveDays(3 * 7, true, false);
+    _moveDays({ day: 3 * 7, forward: true, isL2: false });
 
     uint256 weekNoBefore = locking.getWeek();
 
-    beforeBalances.totalSupply = locking.totalSupply();
-    beforeBalances.pastTotalSupply = locking.getPastTotalSupply(block.number - 3 * L1_WEEK);
-    beforeBalances.balance1 = locking.balanceOf(AIRDROP_CLAIMER_1);
-    beforeBalances.balance2 = locking.balanceOf(AIRDROP_CLAIMER_2);
-    beforeBalances.votingPower1 = locking.getVotes(AIRDROP_CLAIMER_1);
-    beforeBalances.votingPower2 = locking.getVotes(AIRDROP_CLAIMER_2);
-    beforeBalances.pastVotingPower1 = locking.getPastVotes(AIRDROP_CLAIMER_1, block.number - 3 * L1_WEEK);
-    beforeBalances.pastVotingPower2 = locking.getPastVotes(AIRDROP_CLAIMER_2, block.number - 3 * L1_WEEK);
-    beforeBalances.lockedBalance1 = locking.locked(AIRDROP_CLAIMER_1);
-    beforeBalances.lockedBalance2 = locking.locked(AIRDROP_CLAIMER_2);
-    beforeBalances.withdrawable1 = locking.getAvailableForWithdraw(AIRDROP_CLAIMER_1);
-    beforeBalances.withdrawable2 = locking.getAvailableForWithdraw(AIRDROP_CLAIMER_2);
+    // Take snapshot 3 weeks after Nov 07
+    beforeSnapshot = _takeSnapshot(AIRDROP_CLAIMER_1, AIRDROP_CLAIMER_2);
 
     // move 5 weeks forward on L1
-    moveDays(5 * 7, true, false);
+    _moveDays({ day: 5 * 7, forward: true, isL2: false });
 
-    afterBalances.totalSupply = locking.totalSupply();
-    afterBalances.pastTotalSupply = locking.getPastTotalSupply(block.number - 3 * L1_WEEK);
-    afterBalances.balance1 = locking.balanceOf(AIRDROP_CLAIMER_1);
-    afterBalances.balance2 = locking.balanceOf(AIRDROP_CLAIMER_2);
-    afterBalances.votingPower1 = locking.getVotes(AIRDROP_CLAIMER_1);
-    afterBalances.votingPower2 = locking.getVotes(AIRDROP_CLAIMER_2);
-    afterBalances.pastVotingPower1 = locking.getPastVotes(AIRDROP_CLAIMER_1, block.number - 3 * L1_WEEK);
-    afterBalances.pastVotingPower2 = locking.getPastVotes(AIRDROP_CLAIMER_2, block.number - 3 * L1_WEEK);
-    afterBalances.lockedBalance1 = locking.locked(AIRDROP_CLAIMER_1);
-    afterBalances.lockedBalance2 = locking.locked(AIRDROP_CLAIMER_2);
-    afterBalances.withdrawable1 = locking.getAvailableForWithdraw(AIRDROP_CLAIMER_1);
-    afterBalances.withdrawable2 = locking.getAvailableForWithdraw(AIRDROP_CLAIMER_2);
+    // Take snapshot 8 weeks after Nov 07
+    afterSnapshot = _takeSnapshot(AIRDROP_CLAIMER_1, AIRDROP_CLAIMER_2);
 
-    // move 35 days backward on L1
-    moveDays(35, false, false);
+    // move 5 weeks backward on L1
+    _moveDays({ day: 5 * 7, forward: false, isL2: false });
 
-    uint256 blocksTillNextWeek = ((L1_WEEK * (locking.getWeek() + locking.startingPointWeek() + 1)) + 89964) -
-      block.number;
+    uint256 blocksTillNextWeekL1 = _calculateBlocksTillNextWeek({ isL2: false });
 
-    // simulate L2 upgrade
-    vm.prank(mentoLabsMultisig);
-    locking.setL2TransitionBlock(block.number);
+    _simulateL2Upgrade();
 
-    vm.prank(mentoLabsMultisig);
-    locking.setL2StartingPointWeek(20);
-
-    vm.prank(mentoLabsMultisig);
-    locking.setL2Shift(507776);
-
-    vm.prank(mentoLabsMultisig);
-    locking.setPaused(false);
-
-    uint256 blocksTillNextWeek2 = ((L2_WEEK * (locking.getWeek() + uint256(locking.l2StartingPointWeek()) + 1)) +
-      507776) - block.number;
+    uint256 blocksTillNextWeekL2 = _calculateBlocksTillNextWeek({ isL2: true });
 
     // if the shift number is correct, the number of blocks till the next week should be 5 times the previous number
-    assertEq(blocksTillNextWeek2, 5 * blocksTillNextWeek);
+    assertEq(blocksTillNextWeekL2, 5 * blocksTillNextWeekL1);
 
     assertEq(locking.getWeek(), weekNoBefore);
-    assertEq(locking.totalSupply(), beforeBalances.totalSupply);
+    assertEq(locking.totalSupply(), beforeSnapshot.totalSupply);
     // the past values should be calculated using the L1 week value
-    assertEq(locking.getPastTotalSupply(block.number - 3 * L1_WEEK), beforeBalances.pastTotalSupply);
-    assertEq(locking.balanceOf(AIRDROP_CLAIMER_1), beforeBalances.balance1);
-    assertEq(locking.balanceOf(AIRDROP_CLAIMER_2), beforeBalances.balance2);
-    assertEq(locking.getVotes(AIRDROP_CLAIMER_1), beforeBalances.votingPower1);
-    assertEq(locking.getVotes(AIRDROP_CLAIMER_2), beforeBalances.votingPower2);
-    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_1, block.number - 3 * L1_WEEK), beforeBalances.pastVotingPower1);
-    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_2, block.number - 3 * L1_WEEK), beforeBalances.pastVotingPower2);
-    assertEq(locking.locked(AIRDROP_CLAIMER_1), beforeBalances.lockedBalance1);
-    assertEq(locking.locked(AIRDROP_CLAIMER_2), beforeBalances.lockedBalance2);
-    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_1), beforeBalances.withdrawable1);
-    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_2), beforeBalances.withdrawable2);
+    assertEq(locking.getPastTotalSupply(block.number - 3 * L1_WEEK), beforeSnapshot.pastTotalSupply);
+    assertEq(locking.balanceOf(AIRDROP_CLAIMER_1), beforeSnapshot.balance1);
+    assertEq(locking.balanceOf(AIRDROP_CLAIMER_2), beforeSnapshot.balance2);
+    assertEq(locking.getVotes(AIRDROP_CLAIMER_1), beforeSnapshot.votingPower1);
+    assertEq(locking.getVotes(AIRDROP_CLAIMER_2), beforeSnapshot.votingPower2);
+    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_1, block.number - 3 * L1_WEEK), beforeSnapshot.pastVotingPower1);
+    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_2, block.number - 3 * L1_WEEK), beforeSnapshot.pastVotingPower2);
+    assertEq(locking.locked(AIRDROP_CLAIMER_1), beforeSnapshot.lockedBalance1);
+    assertEq(locking.locked(AIRDROP_CLAIMER_2), beforeSnapshot.lockedBalance2);
+    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_1), beforeSnapshot.withdrawable1);
+    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_2), beforeSnapshot.withdrawable2);
 
-    // // move 5 weeks forward on L2
-    moveDays(5 * 7, true, true);
+    // move 5 weeks forward on L2
+    _moveDays({ day: 5 * 7, forward: true, isL2: true });
 
     assertEq(locking.getWeek(), weekNoBefore + 5);
 
-    blocksTillNextWeek2 =
-      ((L2_WEEK * (locking.getWeek() + uint256(locking.l2StartingPointWeek()) + 1)) + 507776) -
-      block.number;
+    blocksTillNextWeekL2 = _calculateBlocksTillNextWeek({ isL2: true });
 
-    assertEq(blocksTillNextWeek2, 5 * blocksTillNextWeek);
-    assertEq(locking.totalSupply(), afterBalances.totalSupply);
-    assertEq(locking.getPastTotalSupply(block.number - 3 * L2_WEEK), afterBalances.pastTotalSupply);
-    assertEq(locking.balanceOf(AIRDROP_CLAIMER_1), afterBalances.balance1);
-    assertEq(locking.balanceOf(AIRDROP_CLAIMER_2), afterBalances.balance2);
-    assertEq(locking.getVotes(AIRDROP_CLAIMER_1), afterBalances.votingPower1);
-    assertEq(locking.getVotes(AIRDROP_CLAIMER_2), afterBalances.votingPower2);
-    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_1, block.number - 3 * L2_WEEK), afterBalances.pastVotingPower1);
-    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_2, block.number - 3 * L2_WEEK), afterBalances.pastVotingPower2);
-    assertEq(locking.locked(AIRDROP_CLAIMER_1), afterBalances.lockedBalance1);
-    assertEq(locking.locked(AIRDROP_CLAIMER_2), afterBalances.lockedBalance2);
-    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_1), afterBalances.withdrawable1);
-    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_2), afterBalances.withdrawable2);
+    assertEq(blocksTillNextWeekL2, 5 * blocksTillNextWeekL1);
+    assertEq(locking.totalSupply(), afterSnapshot.totalSupply);
+    assertEq(locking.getPastTotalSupply(block.number - 3 * L2_WEEK), afterSnapshot.pastTotalSupply);
+    assertEq(locking.balanceOf(AIRDROP_CLAIMER_1), afterSnapshot.balance1);
+    assertEq(locking.balanceOf(AIRDROP_CLAIMER_2), afterSnapshot.balance2);
+    assertEq(locking.getVotes(AIRDROP_CLAIMER_1), afterSnapshot.votingPower1);
+    assertEq(locking.getVotes(AIRDROP_CLAIMER_2), afterSnapshot.votingPower2);
+    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_1, block.number - 3 * L2_WEEK), afterSnapshot.pastVotingPower1);
+    assertEq(locking.getPastVotes(AIRDROP_CLAIMER_2, block.number - 3 * L2_WEEK), afterSnapshot.pastVotingPower2);
+    assertEq(locking.locked(AIRDROP_CLAIMER_1), afterSnapshot.lockedBalance1);
+    assertEq(locking.locked(AIRDROP_CLAIMER_2), afterSnapshot.lockedBalance2);
+    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_1), afterSnapshot.withdrawable1);
+    assertEq(locking.getAvailableForWithdraw(AIRDROP_CLAIMER_2), afterSnapshot.withdrawable2);
+
+    // move 5 days forward on L2
+    _moveDays({ day: 5, forward: true, isL2: true });
+    // we should be at the same week (TUE around 00:00)
+    assertEq(locking.getWeek(), weekNoBefore + 5);
+    // move 1 day forward on L2 + 90 mins as buffer
+    _moveDays({ day: 1, forward: true, isL2: true });
+    vm.roll(block.number + 90 minutes);
+    // we should be at the next week (WED around 01:00)
+    assertEq(locking.getWeek(), weekNoBefore + 6);
+  }
+
+  // takes a snapshot of the locking contract at current block
+  function _takeSnapshot(address claimer1, address claimer2) internal view returns (LockingSnapshot memory snapshot) {
+    snapshot.totalSupply = locking.totalSupply();
+    snapshot.pastTotalSupply = locking.getPastTotalSupply(block.number - 3 * L1_WEEK);
+    snapshot.balance1 = locking.balanceOf(claimer1);
+    snapshot.balance2 = locking.balanceOf(claimer2);
+    snapshot.votingPower1 = locking.getVotes(claimer1);
+    snapshot.votingPower2 = locking.getVotes(claimer2);
+    snapshot.pastVotingPower1 = locking.getPastVotes(claimer1, block.number - 3 * L1_WEEK);
+    snapshot.pastVotingPower2 = locking.getPastVotes(claimer2, block.number - 3 * L1_WEEK);
+    snapshot.lockedBalance1 = locking.locked(claimer1);
+    snapshot.lockedBalance2 = locking.locked(claimer2);
+    snapshot.withdrawable1 = locking.getAvailableForWithdraw(claimer1);
+    snapshot.withdrawable2 = locking.getAvailableForWithdraw(claimer2);
+  }
+
+  // returns the number of blocks till the next week
+  // by calculating the first block of the next week and substracting the current block
+  function _calculateBlocksTillNextWeek(bool isL2) internal view returns (uint256) {
+    if (isL2) {
+      return ((L2_WEEK * (locking.getWeek() + uint256(locking.l2StartingPointWeek()) + 1)) + 507776) - block.number;
+    } else {
+      return ((L1_WEEK * (locking.getWeek() + locking.startingPointWeek() + 1)) + 89964) - block.number;
+    }
+  }
+
+  // simulates the L2 upgrade by setting the necessary parameters
+  function _simulateL2Upgrade() internal {
+    vm.prank(mentoLabsMultisig);
+    locking.setL2TransitionBlock(block.number);
+    vm.prank(mentoLabsMultisig);
+    locking.setL2StartingPointWeek(20);
+    vm.prank(mentoLabsMultisig);
+    locking.setL2Shift(507776);
+    vm.prank(mentoLabsMultisig);
+    locking.setPaused(false);
   }
 
   // move days forward or backward on L1 or L2
-  function moveDays(uint256 day, bool forward, bool isL2) public {
+  function _moveDays(uint256 day, bool forward, bool isL2) internal {
     uint256 ts = vm.getBlockTimestamp();
     uint256 height = vm.getBlockNumber();
 
     uint256 newTs = forward ? ts + day * 1 days : ts - day * 1 days;
+
     uint256 blockChange = isL2 ? (day * 1 days) : ((day * 1 days) / 5);
     uint256 newHeight = forward ? height + blockChange : height - blockChange;
+
     vm.warp(newTs);
     vm.roll(newHeight);
-
-    ts = vm.getBlockTimestamp();
-    height = vm.getBlockNumber();
   }
 }
