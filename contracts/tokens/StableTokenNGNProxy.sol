321b0879bae79135f076fdd3acd71eba3c188719
nvtaveras <4562733+nvtaveras@users.noreply.github.com>
2025-03-11 12:16:02 +0100

    Release v2.6.3 (#581)
    ### Description
    
    New release after adding the new StableTokenXXX proxy contracts so that
    we can use them in mento-deployment
    https://github.com/mento-protocol/mento-core/pull/580
    
    It seems like I messed up the release when I did v2.6.2 since the diff
    shows some commits that should have been merged as part of
    https://github.com/mento-protocol/mento-core/pull/578. Actual diff
    should only be
    https://github.com/mento-protocol/mento-core/commit/0ca06764811c6851d5cc276dc888a811a6a6b617
    
    ---------
    
    Co-authored-by: baroooo <baranseltekin@gmail.com>
    Co-authored-by: philbow61 <philip.raetsch@gmail.com>
    Co-authored-by: Ryan Noble <ryanjnoble@gmail.com>
    Co-authored-by: Bayological <6872903+bayological@users.noreply.github.com>

---
+++ contracts/tokens/StableTokenNGNProxy.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.5.13;
+
+import "celo/contracts/common/Proxy.sol";
+
+/* solhint-disable-next-line no-empty-blocks */
+contract StableTokenNGNProxy is Proxy {}
