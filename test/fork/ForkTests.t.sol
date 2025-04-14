5e79d9412dd8c6743c5ff3408a22f0e7729a4ef1
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2025-04-14 16:04:53 +0200

    test: add FX01 token launches to fork tests

--- test/fork/ForkTests.t.sol
+++ test/fork/ForkTests.t.sol
@@ -91,7 +91,7 @@ contract Alfajores_P0E19_ExchangeForkTest is ExchangeForkTest(ALFAJORES_ID, 0, 1
 
 contract Alfajores_P0E20_ExchangeForkTest is ExchangeForkTest(ALFAJORES_ID, 0, 20) {}
 
-contract Celo_ChainForkTest is ChainForkTest(CELO_ID, 1, uints(17)) {}
+contract Celo_ChainForkTest is ChainForkTest(CELO_ID, 1, uints(21)) {}
 
 contract Celo_P0E00_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 0) {}
 
@@ -126,6 +126,14 @@ contract Celo_P0E14_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 14) {}
 contract Celo_P0E15_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 15) {}
 
 contract Celo_P0E16_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 16) {}
+
+contract Celo_P0E17_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 17) {}
+
+contract Celo_P0E18_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 18) {}
+
+contract Celo_P0E19_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 19) {}
+
+contract Celo_P0E20_ExchangeForkTest is ExchangeForkTest(CELO_ID, 0, 20) {}
 
 contract Celo_BancorExchangeProviderForkTest is BancorExchangeProviderForkTest(CELO_ID) {}
 
