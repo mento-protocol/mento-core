4cf916bafd025a65875750814a4ca820d990c234
philbow61 <philip.raetsch@gmail.com>
2025-06-12 17:41:00 +0200

    feat: add StableTokenV3 implementation

---
+++ contracts/v3/Interfaces/IStableTokenV3.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+interface IStableTokenV3 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address account) external view returns (uint256);
+
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  function allowance(address owner, address spender) external view returns (uint256);
+
+  function approve(address spender, uint256 amount) external returns (bool);
+
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+  function mint(address, uint256) external returns (bool);
+
+  function burn(uint256) external returns (bool);
+
+  function permit(
+    address owner,
+    address spender,
+    uint256 value,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external;
+
+  /**
+   * @notice Transfer token for a specified address
+   * @param to The address to transfer to.
+   * @param value The amount to be transferred.
+   * @param comment The transfer comment.
+   * @return True if the transaction succeeds.
+   */
+  function transferWithComment(address to, uint256 value, string calldata comment) external returns (bool);
+
+  /**
+   * @notice Initializes a StableTokenV2.
+   * It keeps the same signature as the original initialize() function
+   * in legacy/StableToken.sol
+   * @param _name The name of the stable token (English)
+   * @param _symbol A short symbol identifying the token (e.g. "cUSD")
+   * deprecated-param decimals Tokens are divisible to this many decimal places.
+   * deprecated-param registryAddress Address of the Registry contract.
+   * deprecated-param inflationRate Weekly inflation rate.
+   * deprecated-param inflationFactorUpdatePeriod How often the inflation factor is updated, in seconds.
+   * @param initialBalanceAddresses Array of addresses with an initial balance.
+   * @param initialBalanceValues Array of balance values corresponding to initialBalanceAddresses.
+   * deprecated-param exchangeIdentifier String identifier of exchange in registry (for specific fiat pairs)
+   */
+  function initialize(
+    string calldata _name,
+    string calldata _symbol,
+    address[] calldata initialBalanceAddresses,
+    uint256[] calldata initialBalanceValues
+  ) external;
+
+  /**
+   * @notice Initializes a StableTokenV2 contract
+   * when upgrading from legacy/StableToken.sol.
+   * It sets the addresses that were previously read from the Registry.
+   * It runs the ERC20PermitUpgradeable initializer.
+   * @dev This function is only callable once.
+   * @param _broker The address of the Broker contract.
+   * @param _validators The address of the Validators contract.
+   */
+  function initializeV2(address _broker, address _validators) external;
+
+  function setBranchAddresses(
+    address _troveManagerAddress,
+    address _stabilityPoolAddress,
+    address _borrowerOperationsAddress,
+    address _activePoolAddress
+  ) external;
+
+  function setCollateralRegistry(address _collateralRegistryAddress) external;
+
+  /// @notice Gets the address of the Broker contract.
+  function broker() external returns (address);
+
+  /**
+   * @notice Gets the address of the Validators contract.
+   */
+  function validators() external returns (address);
+
+  function debitGasFees(address from, uint256 value) external;
+
+  function creditGasFees(
+    address from,
+    address feeRecipient,
+    address gatewayFeeRecipient,
+    address communityFund,
+    uint256 refund,
+    uint256 tipTxFee,
+    uint256 gatewayFee,
+    uint256 baseTxFee
+  ) external;
+
+  function sendToPool(address _sender, address poolAddress, uint256 _amount) external;
+
+  function returnFromPool(address poolAddress, address user, uint256 _amount) external;
+}
