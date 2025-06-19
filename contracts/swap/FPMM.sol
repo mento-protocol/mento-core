d9bb038e755d13bb1bbb905f7dba891477fd44d3
philbow61 <philip.raetsch@gmail.com>
2025-06-19 10:13:26 +0200

    feat: test reserve fpmm deployment script

--- contracts/swap/FPMM.sol
+++ contracts/swap/FPMM.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.18;
+pragma solidity 0.8.24;
 
 import { IFPMM } from "../interfaces/IFPMM.sol";
 import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
