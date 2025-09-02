efb95b977b8b29770b4d3f013f0b77526fa86f9d
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2025-09-02 11:49:22 +0200

    chore: docs, version, etc

--- contracts/interfaces/IAdaptore.sol
+++ contracts/interfaces/IAdaptore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.24;
+pragma solidity ^0.8.0;
 
 import { IBreakerBox } from "./IBreakerBox.sol";
 import { ISortedOracles } from "./ISortedOracles.sol";
@@ -13,11 +13,13 @@ interface IAdaptore {
   struct AdaptoreStorage {
     // Contract for querying oracle price feeds
     ISortedOracles sortedOracles;
-    // Contract for checking and managing trading modes
+    // Contract for checking trading modes
     IBreakerBox breakerBox;
-    // Contract for checking and managing market hours
+    // Contract for checking market hours
     IMarketHoursBreaker marketHoursBreaker;
   }
+
+  /* ========== EVENTS ========== */
 
   /**
    * @notice Emitted when the SortedOracles contract is updated
@@ -40,6 +42,28 @@ interface IAdaptore {
    */
   event MarketHoursBreakerUpdated(address oldMarketHoursBreaker, address newMarketHoursBreaker);
 
+  /* ========== VARIABLES ========== */
+
+  /**
+   * @notice Returns the contract for oracle price feeds
+   * @return Address of the SortedOracles contract
+   */
+  function sortedOracles() external view returns (ISortedOracles);
+
+  /**
+   * @notice Returns the contract for checking trading modes
+   * @return Address of the BreakerBox contract
+   */
+  function breakerBox() external view returns (IBreakerBox);
+
+  /**
+   * @notice Returns the contract for checking market hours
+   * @return Address of the MarketHoursBreaker contract
+   */
+  function marketHoursBreaker() external view returns (IMarketHoursBreaker);
+
+  /* ========== FUNCTIONS ========== */
+
   /**
    * @notice Initializes the Adaptore contract
    * @param _sortedOracles The address of the sorted oracles contract
@@ -48,17 +72,50 @@ interface IAdaptore {
    */
   function initialize(address _sortedOracles, address _breakerBox, address _marketHoursBreaker) external;
 
-  function sortedOracles() external view returns (ISortedOracles);
+  /**
+   * @notice Sets the address of the sorted oracles contract
+   * @param _sortedOracles The address of the sorted oracles contract
+   */
+  function setSortedOracles(address _sortedOracles) external;
 
-  function breakerBox() external view returns (IBreakerBox);
+  /**
+   * @notice Sets the address of the breaker box contract
+   * @param _breakerBox The address of the breaker box contract
+   */
+  function setBreakerBox(address _breakerBox) external;
 
-  function marketHoursBreaker() external view returns (IMarketHoursBreaker);
+  /**
+   * @notice Sets the address of the market hours breaker contract
+   * @param _marketHoursBreaker The address of the market hours breaker contract
+   */
+  function setMarketHoursBreaker(address _marketHoursBreaker) external;
 
+  /**
+   * @notice Returns true if the market is open
+   * @return true if the market is open, false otherwise
+   */
   function isMarketOpen() external view returns (bool);
 
+  /**
+   * @notice Returns true if the rate for a given rate feed ID is valid
+   * @param rateFeedID The address of the rate feed
+   * @return true if the rate is valid, false otherwise
+   */
   function hasValidRate(address rateFeedID) external view returns (bool);
 
+  /**
+   * @notice Returns the exchange rate for a given rate feed ID
+   * with 18 decimals of precision
+   * @param rateFeedID The address of the rate feed
+   * @return numerator The numerator of the rate
+   * @return denominator The denominator of the rate
+   */
   function getRate(address rateFeedID) external view returns (uint256, uint256);
 
+  /**
+   * @notice Returns the trading mode for a given rate feed ID
+   * @param rateFeedID The address of the rate feed
+   * @return The trading mode
+   */
   function getTradingMode(address rateFeedID) external view returns (uint8);
 }
--- contracts/oracles/Adaptore.sol
+++ contracts/oracles/Adaptore.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.24;
+pragma solidity 0.8.18;
 
 import { IAdaptore } from "../interfaces/IAdaptore.sol";
 import { IBreakerBox } from "../interfaces/IBreakerBox.sol";
@@ -9,25 +9,11 @@ import { IMarketHoursBreaker } from "../interfaces/IMarketHoursBreaker.sol";
 import { OwnableUpgradeable } from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 
 contract Adaptore is IAdaptore, OwnableUpgradeable {
-  // TODO: Make Ownable upgradeable/initializable
-  // Use storage pointer pattern
-  // setters and getters for external contracts
-
   /* ========== CONSTANTS ========== */
 
   // keccak256(abi.encode(uint256(keccak256("mento.storage.Adaptore")) - 1)) & ~bytes32(uint256(0xff))
   bytes32 private constant _ADAPTORE_STORAGE_LOCATION =
     0xd880fc8796ff7fc4b20c6242198b153ac9d227642be16c844e981c5031096c00;
-
-  // struct AdaptoreStorage {
-  //   ISortedOracles sortedOracles;
-  //   IBreakerBox breakerBox;
-  //   IMarketHoursBreaker marketHoursBreaker;
-  // }
-
-  // ISortedOracles public sortedOracles;
-  // IBreakerBox public breakerBox;
-  // IMarketHoursBreaker public marketHoursBreaker;
 
   /* ========== CONSTRUCTOR ========== */
 
@@ -56,25 +42,22 @@ contract Adaptore is IAdaptore, OwnableUpgradeable {
 
   function sortedOracles() external view returns (ISortedOracles) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
-
     return $.sortedOracles;
   }
 
   function breakerBox() external view returns (IBreakerBox) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
-
     return $.breakerBox;
   }
 
   function marketHoursBreaker() external view returns (IMarketHoursBreaker) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
-
     return $.marketHoursBreaker;
   }
 
   /* ========== ADMIN FUNCTIONS ========== */
 
-  // @inheritdoc IAdaptore
+  /// @inheritdoc IAdaptore
   function setSortedOracles(address _sortedOracles) public onlyOwner {
     require(_sortedOracles != address(0), "Adaptore: ZERO_ADDRESS");
 
@@ -85,7 +68,7 @@ contract Adaptore is IAdaptore, OwnableUpgradeable {
     emit SortedOraclesUpdated(oldSortedOracles, _sortedOracles);
   }
 
-  // @inheritdoc IAdaptore
+  /// @inheritdoc IAdaptore
   function setBreakerBox(address _breakerBox) public onlyOwner {
     require(_breakerBox != address(0), "Adaptore: ZERO_ADDRESS");
 
@@ -96,7 +79,7 @@ contract Adaptore is IAdaptore, OwnableUpgradeable {
     emit BreakerBoxUpdated(oldBreakerBox, _breakerBox);
   }
 
-  // @inheritdoc IAdaptore
+  /// @inheritdoc IAdaptore
   function setMarketHoursBreaker(address _marketHoursBreaker) public onlyOwner {
     require(_marketHoursBreaker != address(0), "Adaptore: ZERO_ADDRESS");
 
@@ -109,34 +92,21 @@ contract Adaptore is IAdaptore, OwnableUpgradeable {
 
   /* ========== EXTERNAL FUNCTIONS ========== */
 
-  /**
-   * @notice Returns true if the market is open
-   * @return true if the market is open, false otherwise
-   */
+  /// @inheritdoc IAdaptore
   function isMarketOpen() external view returns (bool) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
 
     return $.marketHoursBreaker.isMarketOpen(block.timestamp);
   }
 
-  /**
-   * @notice Returns the trading mode for a given rate feed ID
-   * @param rateFeedID The address of the rate feed
-   * @return The trading mode
-   */
+  /// @inheritdoc IAdaptore
   function getTradingMode(address rateFeedID) external view returns (uint8) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
 
     return $.breakerBox.getRateFeedTradingMode(rateFeedID);
   }
 
-  /**
-   * @notice Returns the exchange rate for a given rate feed ID
-   * with 18 decimals of precision
-   * @param rateFeedID The address of the rate feed
-   * @return numerator The numerator of the rate
-   * @return denominator The denominator of the rate
-   */
+  /// @inheritdoc IAdaptore
   function getRate(address rateFeedID) external view returns (uint256 numerator, uint256 denominator) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
 
@@ -146,11 +116,7 @@ contract Adaptore is IAdaptore, OwnableUpgradeable {
     denominator = denominator / 1e6;
   }
 
-  /**
-   * @notice Returns true if the rate for a given rate feed ID is valid
-   * @param rateFeedID The address of the rate feed
-   * @return true if the rate is valid, false otherwise
-   */
+  /// @inheritdoc IAdaptore
   function hasValidRate(address rateFeedID) external view returns (bool) {
     AdaptoreStorage storage $ = _getAdaptoreStorage();
 
