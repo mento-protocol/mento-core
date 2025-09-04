9110848e45dd41c3ef8a32af30184c711722133c
Mourad <mourad.kejji@mentolabs.xyz>
2025-09-04 11:01:15 +0200

    feat: add FactoryRegistry
    # Conflicts:
    #	foundry.lock

---
+++ contracts/swap/FactoryRegistry.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.19;
+
+import { OwnableUpgradeable } from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+import { IFactoryRegistry } from "../interfaces/IFactoryRegistry.sol";
+import { EnumerableSet } from "openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
+
+/// @title Protocol Factory Registry
+/// @author Modified from Carter Carlson (@pegahcarter)
+/// @notice Protocol Factory Registry to swap and create gauges
+contract FactoryRegistry is IFactoryRegistry, OwnableUpgradeable {
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  /// @dev The protocol will always have a usable poolFactory.
+  address public immutable FALLBACK_POOL_FACTORY;
+
+  /// @dev Array of poolFactories used to create a gauge and votingRewards
+  EnumerableSet.AddressSet private _poolFactories;
+
+  constructor(address _fallbackPoolFactory) {
+    FALLBACK_POOL_FACTORY = _fallbackPoolFactory;
+
+    approve(_fallbackPoolFactory);
+  }
+
+  /// @inheritdoc IFactoryRegistry
+  function approve(address poolFactory) public onlyOwner {
+    if (poolFactory == address(0)) revert ZeroAddress();
+    if (_poolFactories.contains(poolFactory)) revert PathAlreadyApproved();
+
+    _poolFactories.add(poolFactory);
+    emit Approve(poolFactory);
+  }
+
+  /// @inheritdoc IFactoryRegistry
+  function unapprove(address poolFactory) external onlyOwner {
+    if (poolFactory == FALLBACK_POOL_FACTORY) revert FallbackFactory();
+    if (!_poolFactories.contains(poolFactory)) revert PathNotApproved();
+    _poolFactories.remove(poolFactory);
+    emit Unapprove(poolFactory);
+  }
+
+  /// @inheritdoc IFactoryRegistry
+  function poolFactories() external view returns (address[] memory) {
+    return _poolFactories.values();
+  }
+
+  /// @inheritdoc IFactoryRegistry
+  function isPoolFactoryApproved(address poolFactory) external view returns (bool) {
+    return _poolFactories.contains(poolFactory);
+  }
+
+  /// @inheritdoc IFactoryRegistry
+  function poolFactoriesLength() external view returns (uint256) {
+    return _poolFactories.length();
+  }
+}
