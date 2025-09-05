8cb51704f35a9b0a41d23247dcd78fcdf2910a9b
Bayological <6872903+bayological@users.noreply.github.com>
2025-09-05 13:25:24 -0500

    feat: token agnostic reserve strat

--- contracts/v3/ReserveLiquidityStrategy.sol
+++ contracts/v3/ReserveLiquidityStrategy.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.24;
 
 import { OwnableUpgradeable } from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
-import { ReentrancyGuardUpgradeable } from "openzeppelin-contracts-upgradeable/contracts/security/ReentrancyGuardUpgradeable.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20MintableBurnable } from "../common/IERC20MintableBurnable.sol";
@@ -13,11 +12,9 @@ import { ILiquidityStrategy } from "./Interfaces/ILiquidityStrategy.sol";
 
 /**
  * @title   ReserveLiquidityStrategy
- * @notice  Implements a liquidity strategy that sources liquidity directly
- *          from the Reserve by minting/burning debt tokens and
- *          moving collateral.
+ * @notice  Implements a liquidity strategy that sources liquidity directly from the Reserve.
  */
-contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable {
   using SafeERC20 for IERC20;
 
   /* ============================================================ */
@@ -37,8 +34,8 @@ contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, Ree
   event LiquidityMoved(
     address indexed pool,
     LQ.Direction direction,
-    uint256 debtAmount,
-    uint256 collateralAmount,
+    uint256 tokenInAmount,
+    uint256 tokenOutAmount,
     uint256 incentiveAmount
   );
 
@@ -56,7 +53,6 @@ contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, Ree
    */
   function initialize(address _reserve, address _owner) external initializer {
     __Ownable_init();
-    __ReentrancyGuard_init();
 
     require(_reserve != address(0), "RLS: INVALID_RESERVE");
     require(_owner != address(0), "RLS: INVALID_OWNER");
@@ -104,7 +100,7 @@ contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, Ree
    * @param action The action to execute
    * @return ok True if execution succeeded
    */
-  function execute(LQ.Action calldata action) external nonReentrant returns (bool ok) {
+  function execute(LQ.Action calldata action) external returns (bool ok) {
     require(action.liquiditySource == LQ.LiquiditySource.Reserve, "RLS: WRONG_SOURCE");
     require(action.pool != address(0), "RLS: INVALID_POOL");
     require(trustedPools[action.pool], "RLS: UNTRUSTED_POOL");
@@ -120,8 +116,8 @@ contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, Ree
     emit LiquidityMoved(
       action.pool,
       action.dir,
-      action.dir == LQ.Direction.Expand ? action.inputAmount : action.amount0Out + action.amount1Out,
-      action.dir == LQ.Direction.Contract ? action.inputAmount : action.amount0Out + action.amount1Out,
+      action.inputAmount,
+      action.amount0Out + action.amount1Out,
       incentiveAmount
     );
 
@@ -144,25 +140,15 @@ contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, Ree
       (uint256, uint256, LQ.Direction, bool)
     );
 
-    if (direction == LQ.Direction.Expand) {
-      _handleExpansionCallback(
-        msg.sender, // pool address
-        amount0Out,
-        amount1Out,
-        inputAmount,
-        incentiveAmount,
-        isToken0Debt
-      );
-    } else {
-      _handleContractionCallback(
-        msg.sender, // pool address
-        amount0Out,
-        amount1Out,
-        inputAmount,
-        incentiveAmount,
-        isToken0Debt
-      );
-    }
+    _handleRebalanceCallback(
+      msg.sender,
+      amount0Out,
+      amount1Out,
+      inputAmount,
+      incentiveAmount,
+      direction,
+      isToken0Debt
+    );
   }
 
   /* ============================================================ */
@@ -170,69 +156,105 @@ contract ReserveLiquidityStrategy is ILiquidityStrategy, OwnableUpgradeable, Ree
   /* ============================================================ */
 
   /**
-   * @notice Handle expansion callback: mint debt to pool, move collateral to reserve
-   * @dev Pool price > oracle price. Move collateral OUT, debt IN.
+   * @notice Handle rebalance callback: provide tokens IN to pool, receive tokens OUT
+   * @dev Handles both expansion and contraction directions
+   * @param pool The pool address
+   * @param amount0Out Amount of token0 coming out of the pool
+   * @param amount1Out Amount of token1 coming out of the pool
+   * @param inputAmount Total amount going into the pool (including incentive)
+   * @param incentiveAmount Incentive amount for the rebalancer
+   * @param direction The rebalance direction (Expand or Contract)
+   * @param isToken0Debt Whether token0 is the debt token
    */
-  function _handleExpansionCallback(
+  function _handleRebalanceCallback(
     address pool,
     uint256 amount0Out,
     uint256 amount1Out,
     uint256 inputAmount,
     uint256 incentiveAmount,
+    LQ.Direction direction,
     bool isToken0Debt
   ) internal {
     (, , , , address token0, address token1) = IFPMM(pool).metadata();
-    address debtTokenAddr = isToken0Debt ? token0 : token1;
-    address collateralTokenAddr = isToken0Debt ? token1 : token0;
+    
+    address tokenIn;
+    address tokenOut;
+    uint256 amountOut;
+    
+    if (direction == LQ.Direction.Expand) {
+      // Expansion: Pool price > oracle price
+      // Reserve provides debt to pool, receives collateral from pool
+      tokenIn = isToken0Debt ? token0 : token1;  // debt
+      tokenOut = isToken0Debt ? token1 : token0; // collateral
+      amountOut = isToken0Debt ? amount1Out : amount0Out;
+    } else {
+      // Contraction: Pool price < oracle price
+      // Reserve provides collateral to pool, receives debt from pool
+      tokenIn = isToken0Debt ? token1 : token0;  // collateral
+      tokenOut = isToken0Debt ? token0 : token1; // debt
+      amountOut = isToken0Debt ? amount0Out : amount1Out;
+    }
 
-    IERC20MintableBurnable debtToken = IERC20MintableBurnable(debtTokenAddr);
-    IERC20 collateralToken = IERC20(collateralTokenAddr);
-
-    // Mint debt tokens to the pool
-    uint256 debtToMint = inputAmount - incentiveAmount;
-    debtToken.mint(pool, debtToMint);
-
-    // Mint incentive to the strategy
-    debtToken.mint(address(this), incentiveAmount);
-
-    // Transfer collateral to the reserve
-    uint256 collateralAmount = isToken0Debt ? amount1Out : amount0Out;
-    collateralToken.safeTransfer(address(reserve), collateralAmount);
+    // Handle token going INTO the pool
+    uint256 amountToPool = inputAmount - incentiveAmount;
+    _transferTokenIn(tokenIn, pool, amountToPool, incentiveAmount);
+    
+    // Handle token coming OUT of the pool
+    _transferTokenOut(tokenOut, amountOut);
   }
 
   /**
-   * @notice Handle contraction callback: burn debt from pool, provide collateral from reserve
-   * @dev Pool price < oracle price. Move debt OUT, collateral IN.
+   * @notice Transfer tokens into the pool and pay incentive
+   * @param token The token to transfer in
+   * @param pool The pool to transfer to
+   * @param amountToPool Amount to send to the pool
+   * @param incentiveAmount Incentive amount to send to this contract
    */
-  function _handleContractionCallback(
+  function _transferTokenIn(
+    address token,
     address pool,
-    uint256 amount0Out,
-    uint256 amount1Out,
-    uint256 inputAmount,
-    uint256 incentiveAmount,
-    bool isToken0Debt
+    uint256 amountToPool,
+    uint256 incentiveAmount
   ) internal {
-    (, , , , address token0, address token1) = IFPMM(pool).metadata();
-    address debtTokenAddr = isToken0Debt ? token0 : token1;
-    address collateralTokenAddr = isToken0Debt ? token1 : token0;
+    if (reserve.isStableAsset(token)) {
+      // Mint stable assets directly
+      IERC20MintableBurnable(token).mint(pool, amountToPool);
+      if (incentiveAmount > 0) {
+        IERC20MintableBurnable(token).mint(address(this), incentiveAmount);
+      }
+    } else if (reserve.isCollateralAsset(token)) {
+      // Transfer collateral from reserve
+      require(
+        reserve.transferExchangeCollateralAsset(token, payable(pool), amountToPool),
+        "RLS: COLLATERAL_TO_POOL_FAILED"
+      );
+      if (incentiveAmount > 0) {
+        require(
+          reserve.transferExchangeCollateralAsset(token, payable(address(this)), incentiveAmount),
+          "RLS: INCENTIVE_TRANSFER_FAILED"
+        );
+      }
+    } else {
+      revert("RLS: TOKEN_IN_NOT_SUPPORTED");
+    }
+  }
 
-    IERC20MintableBurnable debtToken = IERC20MintableBurnable(debtTokenAddr);
-
-    // Burn debt tokens received from the pool
-    uint256 debtAmount = isToken0Debt ? amount0Out : amount1Out;
-    debtToken.burn(debtAmount);
-
-    // Transfer collateral from reserve to pool
-    uint256 collateralToPoolAmount = inputAmount - incentiveAmount;
-    require(
-      reserve.transferExchangeCollateralAsset(collateralTokenAddr, payable(pool), collateralToPoolAmount),
-      "RLS: COLLATERAL_TRANSFER_FAILED"
-    );
-
-    // Transfer incentive from reserve to the strategy
-    require(
-      reserve.transferExchangeCollateralAsset(collateralTokenAddr, payable(address(this)), incentiveAmount),
-      "RLS: INCENTIVE_TRANSFER_FAILED"
-    );
+  /**
+   * @notice Transfer tokens out from the pool back to reserve
+   * @param token The token to transfer out
+   * @param amount The amount to transfer
+   */
+  function _transferTokenOut(address token, uint256 amount) internal {
+    if (amount == 0) return;
+    
+    if (reserve.isStableAsset(token)) {
+      // Burn stable assets received from pool
+      IERC20MintableBurnable(token).burn(amount);
+    } else if (reserve.isCollateralAsset(token)) {
+      // Transfer collateral back to reserve
+      IERC20(token).safeTransfer(address(reserve), amount);
+    } else {
+      revert("RLS: TOKEN_OUT_NOT_SUPPORTED");
+    }
   }
 }
