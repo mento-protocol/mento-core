06983208c0555689d37bff00ddb540db604d0348
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2025-09-16 14:47:13 +0200

    fix: cleanup market hours breaker, simplify interface and tests

--- contracts/interfaces/IMarketHoursBreaker.sol
+++ contracts/interfaces/IMarketHoursBreaker.sol
@@ -1,20 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.24;
 
-import { IBreaker } from "./IBreaker.sol";
+interface IMarketHoursBreaker {
+  /**
+   * @notice Check if the timestamp is inside fx market hours
+   * @param timestamp The timestamp to check
+   * @return True if the timestamp is inside fx market hours, false otherwise
+   */
+  function isMarketOpen(uint256 timestamp) external pure returns (bool);
 
-interface IRevertBreaker {
-  function shouldTrigger(address rateFeedID) external returns (bool triggerBreaker);
-}
-
-interface IMarketHoursBreaker is IRevertBreaker {
-  function isMarketOpen(uint256 timestamp) external view returns (bool);
-
-  // function getCoolDown(address rateFeedID) external view returns (uint256);
-
-  // function setCooldownTimes(address[] calldata rateFeedIDs, uint256[] calldata cooldownTimes) external;
-
-  // function setDefaultCooldownTime(uint256 cooldownTime) external;
-
-  // function defaultCooldownTime() external view returns (uint256);
+  /**
+   * @notice  Enforces that the market is open during valid trading hours.
+   *          This function reverts if called outside of fx market hours or on holidays.
+   * @param   rateFeedID The rate feed to be checked. Unused in this implementation as market hours
+   *          in this breaker are not rate feed dependent.
+   * @return  triggerBreaker Always returns false if execution completes (market is open),
+   *          And reverts if the market is closed.
+   * @dev     This function implements the IBreaker interface but uses a revert-on-condition
+   *          pattern rather than returning true/false. The boolean return is required by the
+   *          interface but will only be reached when the condition passes (market open).
+   */
+  function shouldTrigger(address rateFeedID) external view returns (bool triggerBreaker);
 }
--- contracts/oracles/breakers/MarketHoursBreaker.sol
+++ contracts/oracles/breakers/MarketHoursBreaker.sol
@@ -1,44 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.24;
 
-// import { IBreaker } from "../../interfaces/IBreaker.sol";
 import { IMarketHoursBreaker } from "../../interfaces/IMarketHoursBreaker.sol";
-import { Ownable } from "openzeppelin-contracts-next/contracts/access/Ownable.sol";
 
-import { BokkyPooBahsDateTimeLibrary } from "BokkyPooBahsDateTimeLibrary/contracts/BokkyPooBahsDateTimeLibrary.sol";
-// import { WithCooldownV2 } from "./WithCooldownV2.sol";
+import { BokkyPooBahsDateTimeLibrary as DateTimeLibrary } from "BokkyPooBahsDateTimeLibrary/contracts/BokkyPooBahsDateTimeLibrary.sol";
 
+/**
+ * @title MarketHoursBreaker
+ * @notice A special type of breaker that reverts if called outside of fx market hours or on holidays.
+ *         Used to enforce that FX rates are only being reported during valid trading hours.
+ */
 contract MarketHoursBreaker is IMarketHoursBreaker {
-  /* ========== CONSTRUCTOR ========== */
-  /**
-   * @notice Contract constructor
-   */
-  constructor() {
-    // _transferOwnership(msg.sender);
-  }
-
   /* ========== VIEW FUNCTIONS ========== */
 
-  /**
-   * @notice Check if the timestamp is inside fx market hours
-   * @param timestamp The timestamp to check
-   * @return True if the timestamp is inside fx market hours, false otherwise
-   */
-  function isMarketOpen(uint256 timestamp) public view returns (bool) {
+  /// @inheritdoc IMarketHoursBreaker
+  function isMarketOpen(uint256 timestamp) public pure returns (bool) {
     return !_isWeekendHours(timestamp) && !_isHoliday(timestamp);
   }
 
-  /* ========== EXTERNAL FUNCTIONS ========== */
-
-  /**
-   * @notice  Check if the timestamp is outside of fx market hours or on a holiday,
-   *          in which case the breaker will be triggered.
-   * @param   rateFeedID The rate feed to be checked. Unused in this implementation as market hours
-   *          in this breaker are not rate feed dependent.
-   * @return  triggerBreaker True if the timestamp is outside of fx market hours or on a holiday, false otherwise
-   */
+  /// @inheritdoc IMarketHoursBreaker
   // solhint-disable-next-line no-unused-vars
-  function shouldTrigger(address rateFeedID) public returns (bool triggerBreaker) {
+  function shouldTrigger(address rateFeedID) public view returns (bool triggerBreaker) {
     require(isMarketOpen(block.timestamp), "MarketHoursBreaker: Market is closed");
 
     return false;
@@ -51,9 +33,9 @@ contract MarketHoursBreaker is IMarketHoursBreaker {
    * @param timestamp The timestamp to check
    * @return True if the timestamp is during FX weekend hours, false otherwise
    */
-  function _isWeekendHours(uint256 timestamp) internal view returns (bool) {
-    uint256 dow = BokkyPooBahsDateTimeLibrary.getDayOfWeek(timestamp);
-    uint256 hour = BokkyPooBahsDateTimeLibrary.getHour(timestamp);
+  function _isWeekendHours(uint256 timestamp) internal pure returns (bool) {
+    uint256 dow = DateTimeLibrary.getDayOfWeek(timestamp);
+    uint256 hour = DateTimeLibrary.getHour(timestamp);
 
     // slither-disable-start incorrect-equality
     bool isFridayEvening = dow == 5 && hour >= 21;
@@ -69,9 +51,9 @@ contract MarketHoursBreaker is IMarketHoursBreaker {
    * @param timestamp The timestamp to check
    * @return True if the timestamp is during FX holidays, false otherwise
    */
-  function _isHoliday(uint256 timestamp) internal view returns (bool) {
-    uint256 month = BokkyPooBahsDateTimeLibrary.getMonth(timestamp);
-    uint256 day = BokkyPooBahsDateTimeLibrary.getDay(timestamp);
+  function _isHoliday(uint256 timestamp) internal pure returns (bool) {
+    uint256 month = DateTimeLibrary.getMonth(timestamp);
+    uint256 day = DateTimeLibrary.getDay(timestamp);
 
     // slither-disable-next-line incorrect-equality
     return ((month == 12 && day == 25) || (month == 1 && day == 1));
