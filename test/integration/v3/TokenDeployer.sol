94447242d2288b1e111eac77959a3c889fbe753e
Mouradif <mkejji@gmail.com>
2025-10-18 12:38:05 +0200

    feat: add TokenDeployer

---
+++ test/integration/v3/TokenDeployer.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// solhint-disable max-line-length, function-max-lines
+
+pragma solidity 0.8.24;
+import { TestStorage } from "./TestStorage.sol";
+
+import { IBoldToken, IERC20Metadata } from "bold/src/Interfaces/IBoldToken.sol";
+import { MockERC20 } from "test/utils/mocks/MockERC20.sol";
+import { IStableTokenV3 } from "contracts/interfaces/IStableTokenV3.sol";
+import { StableTokenV3 } from "contracts/tokens/StableTokenV3.sol";
+
+contract TokenDeployer is TestStorage {
+  function _deployCollateralToken(string memory name, string memory symbol, uint8 decimals) internal {
+    $tokens.collateralToken = IStableTokenV3(address(new MockERC20(name, symbol, decimals)));
+  }
+
+  function _deployDebtToken(string memory name, string memory symbol) internal {
+    StableTokenV3 newDebtToken = new StableTokenV3(false);
+    uint256[] memory numbers = new uint256[](0);
+    address[] memory addresses = new address[](0);
+    newDebtToken.initialize(name, symbol, addresses, numbers, addresses, addresses, addresses);
+    $tokens.debtToken = IBoldToken(address(newDebtToken));
+  }
+}
