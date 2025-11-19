486889ea39e301523263d8f94be50f7cc0972463
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2025-11-19 15:46:11 +0100

    feat: add new whenRecipientIsDifferentFromSender test

--- test/unit/swap/FPMM/FPMMMint.t.sol
+++ test/unit/swap/FPMM/FPMMMint.t.sol
@@ -105,6 +105,39 @@ contract FPMMMintTest is FPMMBaseTest {
     vm.stopPrank();
   }
 
+  function test_mint_whenRecipientIsDifferentFromSender_shouldCalculateLiquidityCorrectly()
+    public
+    initializeFPMM_withDecimalTokens(18, 18)
+    mintInitialLiquidity(18, 18)
+  {
+    uint256 amount0 = 50e18;
+    uint256 amount1 = 100e18;
+
+    uint256 initialReserve0 = fpmm.reserve0();
+    uint256 initialReserve1 = fpmm.reserve1();
+
+    uint256 aliceInitialBalance = fpmm.balanceOf(ALICE);
+
+    vm.startPrank(BOB);
+    IERC20(token0).transfer(address(fpmm), amount0);
+    IERC20(token1).transfer(address(fpmm), amount1);
+
+    uint256 expectedLiquidity = 141421356237309504880 / 2; // half of the initial liquidity
+
+    vm.expectEmit(true, true, true, true);
+    emit Mint(BOB, amount0, amount1, expectedLiquidity, ALICE);
+    uint256 liquidity = fpmm.mint(ALICE);
+
+    assertEq(liquidity, expectedLiquidity);
+    assertEq(fpmm.balanceOf(ALICE), aliceInitialBalance + expectedLiquidity);
+    assertEq(fpmm.totalSupply(), 3 * expectedLiquidity);
+
+    assertEq(fpmm.reserve0(), initialReserve0 + amount0);
+    assertEq(fpmm.reserve1(), initialReserve1 + amount1);
+
+    vm.stopPrank();
+  }
+
   function test_mint_whenDecimalsAreDifferent_shouldCalculateLiquidityCorrectly()
     public
     initializeFPMM_withDecimalTokens(18, 6)
