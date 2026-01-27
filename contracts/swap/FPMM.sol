37af6eadc3606b1ba442c74f0b4afd14999f447a
Nelson Taveras <4562733+nvtaveras@users.noreply.github.com>
2026-01-27 15:45:09 +0100

    fix: add denominator check

--- contracts/swap/FPMM.sol
+++ contracts/swap/FPMM.sol
@@ -763,6 +763,7 @@ contract FPMM is IRPool, IFPMM, ReentrancyGuardUpgradeable, ERC20Upgradeable, Ow
     FPMMStorage storage $ = _getFPMMStorage();
 
     (rateNumerator, rateDenominator) = $.oracleAdapter.getFXRateIfValid($.referenceRateFeedID);
+    assert(rateDenominator == 1e18); // denominator from sorted oracles should always be 1e18
 
     if ($.invertRateFeed) {
       (rateNumerator, rateDenominator) = (rateDenominator, rateNumerator);
