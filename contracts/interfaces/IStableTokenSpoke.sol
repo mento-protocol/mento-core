70a5294f21b966d826078e695bd42ac025d4404d
nvtaveras <4562733+nvtaveras@users.noreply.github.com>
2026-03-09 17:35:38 +0100

    fix: update IStableTokenSpoke sol version to 0.8 (#712)

--- contracts/interfaces/IStableTokenSpoke.sol
+++ contracts/interfaces/IStableTokenSpoke.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.24;
+pragma solidity ^0.8.0;
 import {
   IERC20PermitUpgradeable
 } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-IERC20PermitUpgradeable.sol";
